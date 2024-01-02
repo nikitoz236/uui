40a8376814ba8714f5d9a7d9c11cd31bce79e28e
Nikita Kulikov <nikitoz236@gmail.com>
2024-01-02 16:10:16 +0300

    add macro for create tree

--- main.c
+++ main.c
@@ -109,6 +109,51 @@ widget_t union_widget = {
 
 
 
+#define WIDGET_UNION(hm, ho, vm, vo, n1, n2)  \
+    { \
+        .widget = &union_widget, \
+        .widget_cfg = &(union_widget_cfg_t){ \
+            .nodes = { n1, n2 }, \
+            .align_mode = { \
+                .h = { \
+                    .mode = ALIGN_ ## hm, \
+                    .offset = ho \
+                }, \
+                .v = { \
+                    .mode = ALIGN_ ## vm, \
+                    .offset = vo \
+                } \
+            } \
+        } \
+    }
+
+#define WIDGET_RECT(sw, sh, c) \
+    { \
+        .widget = &rect_widget, \
+        .widget_cfg = &(rect_widget_cfg_t){ \
+            .size = { \
+                .w = sw, \
+                .h = sh \
+            }, \
+            .color = c \
+        } \
+    }
+
+#define ALIGN_MODE() \
+
+
+ui_node_t ui_test =
+    WIDGET_UNION( CENTER, 0, UP_OUTSIDE, 0,
+        WIDGET_RECT( 200, 300, 0xff0000 ),
+        WIDGET_UNION( CENTER, 0, UP_OUTSIDE, 0,
+            WIDGET_RECT( 300, 50, 0x00ff00 ),
+            WIDGET_UNION( CENTER, 0, UP_OUTSIDE, 0,
+                WIDGET_RECT( 100, 100, 0x0000ff ),
+                WIDGET_RECT( 100, 100, 0x0ffff0 )
+            )
+        )
+    )
+;
 
 
 
@@ -125,10 +170,34 @@ ui_node_t ui_root = {
                 }
             },
             {
-                .widget = &rect_widget,
-                .widget_cfg = &(rect_widget_cfg_t){
-                    .size = { 50, 100 },
-                    .color = 0x0000ff
+                .widget = &union_widget,
+                .widget_cfg = &(union_widget_cfg_t){
+                    .nodes = {
+                        {
+                            .widget = &rect_widget,
+                            .widget_cfg = &(rect_widget_cfg_t){
+                                .size = { 300, 50 },
+                                .color = 0x00ff00
+                            }
+                        },
+                        {
+                            .widget = &rect_widget,
+                            .widget_cfg = &(rect_widget_cfg_t){
+                                .size = { 100, 100 },
+                                .color = 0x0000ff
+                            }
+                        }
+                    },
+                    .align_mode = {
+                        .h = {
+                            .mode = ALIGN_CENTER,
+                            .offset = 0
+                        },
+                        .v = {
+                            .mode = ALIGN_CENTER,
+                            .offset = 0
+                        }
+                    }
                 }
             }
         },
@@ -151,11 +220,12 @@ ui_node_t ui_root = {
 
 int main() {
     ui_ctx_t * ui_ctx = (ui_ctx_t *)ui_stack;
-    calc_node(&ui_root, ui_ctx, 0);
-
+    ui_node_t * node = &ui_test;
+    calc_node(node, ui_ctx, 0);
     gfx_open(ui_ctx->f.s.x, ui_ctx->f.s.y, "UC lib gfx emu device");
-
-    draw_node(&ui_root, ui_ctx);
+    draw_node(node, ui_ctx);
+    printf("ui ctx struct size: %ld\r\n", sizeof(ui_ctx_t));
+    printf("ui ctx size: %d\r\n", ui_ctx->size);
 
     while (1) {
         char kbd_btn = gfx_routine();
