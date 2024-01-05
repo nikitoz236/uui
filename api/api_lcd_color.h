19558902b928cf7c83f52a6a332c577ba3682db4
Nikita Kulikov <nikitoz236@gmail.com>
2024-01-05 10:59:08 +0300

    api_lcd_color

--- api/api_lcd_color.h
+++ api/api_lcd_color.h
@@ -1,6 +1,5 @@
 #include <stdint.h>
+#include "color_type.h"
 
 void lcd_rect(unsigned x, unsigned y, unsigned w, unsigned h, unsigned color);
-void lcd_bitmap(unsigned x, unsigned y, unsigned w, unsigned h, unsigned scale, void * buf);
-
-void lcd_refresh(void);
+void lcd_image(unsigned x, unsigned y, unsigned w, unsigned h, unsigned scale, lcd_color_t * buf);
