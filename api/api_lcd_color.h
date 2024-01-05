10559f844ec655cf72c6d326ff1773a79a0cec35
Nikita Kulikov <nikitoz236@gmail.com>
2024-01-05 11:11:55 +0300

    api_lcd_color comments

--- api/api_lcd_color.h
+++ api/api_lcd_color.h
@@ -1,5 +1,14 @@
 #include <stdint.h>
 #include "color_type.h"
 
+/*
+    интерфейс для работы с цветным дисплеем
+
+    функции для работы графики, которые нам должен обеспечить драйвер
+
+    разделены на 2 ради оптимизации, одна заливает область цветом, другая изображением
+    внутри могут быть как угодно оптимизированы, использовать dma итд
+*/
+
 void lcd_rect(unsigned x, unsigned y, unsigned w, unsigned h, unsigned color);
 void lcd_image(unsigned x, unsigned y, unsigned w, unsigned h, unsigned scale, lcd_color_t * buf);
