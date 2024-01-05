5d2ba8abd1b3fe5ca0a0e5e0f6c8652655ca3365
Nikita Kulikov <nikitoz236@gmail.com>
2024-01-05 10:59:37 +0300

    add emu src paths

--- emu/emu.mk
+++ emu/emu.mk
@@ -4,8 +4,16 @@
 
 INC += $(UCLIB_PATH)/emu
 INC += $(UCLIB_PATH)/forms
+INC += $(UCLIB_PATH)/widgets
+INC += $(UCLIB_PATH)/api
+INC += $(UCLIB_PATH)/fonts
+INC += $(UCLIB_PATH)/common
+
 EMU_SRC += $(UCLIB_PATH)/emu/*.c
 LIB_SRC += $(UCLIB_PATH)/forms/*.c
+LIB_SRC += $(UCLIB_PATH)/widgets/*.c
+LIB_SRC += $(UCLIB_PATH)/fonts/*.c
+# LIB_SRC += $(UCLIB_PATH)/common/*.c
 GFX_FLAGS = -lX11 -lm -I /usr/X11R6/include
 
 emu: emu_app
