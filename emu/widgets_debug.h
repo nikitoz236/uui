593a1c3bf605451c0f8d727d603adff1f2648587
Nikita Kulikov <nikitoz236@gmail.com>
2024-01-14 07:52:04 +0300

    add form print debug

--- emu/widgets_debug.h
+++ emu/widgets_debug.h
@@ -1,3 +1,10 @@
+#pragma once
 #include "widgets.h"
+#include <stdio.h>
 
 void widgets_print_ctx(unsigned level, ui_ctx_t * ctx, unsigned max_num);
+
+static inline void form_print(char * name, form_t * f)
+{
+    printf("%s: p(%d, %d), s(%d, %d)\n", name, f->p.x, f->p.y, f->s.x, f->s.y);
+}
