8373bb760623f8310afe2af522837858d0e13d26
Nikita Kulikov <nikitoz236@gmail.com>
2024-02-10 18:42:15 +0300

    lib/common/mstimer and array_size

---
+++ lib/common/mstimer.h
@@ -0,0 +1,60 @@
+#pragma once
+#include <stdint.h>
+
+extern unsigned uptime_ms;
+
+typedef struct {
+    unsigned timeout;
+    unsigned start;
+} mstimer_t;
+
+static inline mstimer_t mstimer_with_timeout(unsigned timeout)
+{
+    return (mstimer_t){ .timeout = timeout };
+}
+
+
+static inline void mstimer_set_timeout(mstimer_t * t, unsigned timeout)
+{
+    t->timeout = timeout;
+}
+
+static inline void mstimer_reset(mstimer_t * t)
+{
+    t->start = uptime_ms;
+}
+
+static inline void mstimer_start_timeout(mstimer_t * t, unsigned timeout)
+{
+    t->timeout = timeout;
+    t->start = uptime_ms;
+}
+
+static inline unsigned mstimer_get_time(mstimer_t * t)
+{
+    return uptime_ms - t->start;
+}
+
+static inline int __time_rel(unsigned t, unsigned ref)
+{
+    // > 0 when t before ref
+    // < 0 when t after ref
+    return (int)(ref - t);
+}
+
+static inline unsigned mstimer_is_over(mstimer_t * t)
+{
+    if (__time_rel(t->start + t->timeout, uptime_ms) < 0) {
+        return 1;
+    }
+    return 0;
+}
+
+static inline unsigned mstimer_do_period(mstimer_t * t)
+{
+    if (__time_rel(t->start + t->timeout, uptime_ms) < 0) {
+        t->start += t->timeout;
+        return 1;
+    }
+    return 0;
+}
