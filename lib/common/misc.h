6aeda8bd22739b71fc44a04145a534ce3711eb9e
Nikita Kulikov <nikitoz236@gmail.com>
2024-02-10 08:28:12 +0300

    round_up_deg2

--- lib/common/misc.h
+++ lib/common/misc.h
@@ -2,3 +2,8 @@
 #include <stdint.h>
 
 #define DIV_ROUND_UP(x, d)                      ((x + (d - 1)) / d)
+
+static inline unsigned round_up_deg2(unsigned val, unsigned step)
+{
+    return (val + (step - 1)) & ~(step - 1);
+}
