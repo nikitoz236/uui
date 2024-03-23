e7890311783d15da8aa95a6fa442cc2b7e6e4dff
Nikita Kulikov <nikitoz236@gmail.com>
2024-03-23 14:51:49 +0300

    emu hw rtc return seconds from 2000 year

--- lib/emu/hw/rtc.c
+++ lib/emu/hw/rtc.c
@@ -5,7 +5,7 @@ static unsigned rtc_time_s = 0;
 
 void init_rtc(void)
 {
-    clock_gettime(CLOCK_REALTIME, &rtc_start);
+    // clock_gettime(CLOCK_REALTIME, &rtc_start);
 }
 
 unsigned rtc_get_time_s(void)
@@ -13,16 +13,7 @@ unsigned rtc_get_time_s(void)
     struct timespec t;
     clock_gettime(CLOCK_REALTIME, &t);
     unsigned uptime_s = t.tv_sec - rtc_start.tv_sec;
-    return rtc_time_s + uptime_s;
-}
-
-unsigned systick_get_uptime_ms(void)
-{
-    struct timespec t;
-    clock_gettime(CLOCK_REALTIME, &t);
-    unsigned uptime_s = t.tv_sec - rtc_start.tv_sec;
-    unsigned ms = (t.tv_nsec - rtc_start.tv_nsec) / 1000000;
-    return uptime_s * 1000 + ms;
+    return rtc_time_s + uptime_s - 946674000;
 }
 
 void rtc_set_time_s(unsigned time_s)
