b2821952fd7e20e86387abca269a04aa5fcb3d7d
Nikita Kulikov <nikitoz236@gmail.com>
2024-03-23 14:54:15 +0300

    lib hw flash: add flash atomic write and atomic erase const to flash_atomic.h

--- lib/hw/include/flash_hw.h
+++ lib/hw/include/flash_hw.h
@@ -1,9 +1,6 @@
-#include <stdint.h>
+#include "flash_atomic.h"
 
-typedef uint16_t flash_wr_t;
-
-#define FLASH_ATOMIC_ERASE_SIZE     1024
 #define FLASH_ERASED                ((flash_wr_t)(-1))
 
 void flash_erase_page(unsigned page);
-void flash_write(void * addr, const void * data, unsigned size);
+void flash_write(const void * addr, const void * data, unsigned size);
