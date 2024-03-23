4a60f987ac87a57038239a5bce3d2ef3fd39894a
Nikita Kulikov <nikitoz236@gmail.com>
2024-03-23 16:36:14 +0300

    str_val fix when value 0

--- lib/common/str_val.c
+++ lib/common/str_val.c
@@ -1,8 +1,16 @@
+/*
+    либа для печати десятичного представления в текстовый буфер
 
+
+*/
 
 void dec_to_str_right_aligned(unsigned val, char * str, unsigned len, unsigned lzero)
 {
     str[len] = 0;
+    if (val == 0) {
+        str[len - 1] = '0';
+        return;
+    }
     while (len) {
         len--;
         if (val) {
