f737fffd5ec6899341b09598567a8d1b32cdffa8
Nikita Kulikov <nikitoz236@gmail.com>
2024-03-31 09:46:01 +0300

    rework widget_titled_screen - focus no redraw child

--- demo/honda_tc/tests/graphic/titled_screen/widget_titled_screen.c
+++ demo/honda_tc/tests/graphic/titled_screen/widget_titled_screen.c
@@ -12,46 +12,22 @@ extern font_t font_5x7;
 typedef struct {
     uint8_t current_idx;
     uint8_t focus;
+    form_t text_box;
+    form_t text_form;
 } __widget_titled_screen_ctx_t;
 
-static inline void create_title(char * title, color_scheme_t * cs, form_t * pf, form_t * sf)
-{
-    /*
-        также посчитает размер формы sf оставшегося доступного окна
-    */
+static const lcd_text_cfg_t text_cfg = {
+    .font = &font_5x7,
+    .text_size = { .w = 20, .h = 1 },
+    .gaps = { 1, 1},
+    .scale = 1
+};
 
-    lcd_text_cfg_t text_cfg = {
-        .font = &font_5x7,
-        .text_size = { .w = str_len(title, 20), .h = 1 },
-        .gaps = { 1, 1},
-        .scale = 1
-    };
-
-    form_t text_form;
-    lcd_text_calc_size(&text_form.s, &text_cfg);
-
-    form_t text_box = *pf;
-    form_cut(&text_box, text_form.s.h + (2 * MARGIN), DIMENSION_HEIGHT, EDGE_D);
-
-    form_fill(&text_box, cs->bg);
-
-    form_align(pf, &text_form, &ALIGN_MODE(LI, 6, UI, MARGIN));
-    lcd_text_color_print(title, &text_form.p, &text_cfg, cs, 0, 0, 0);
-
-    *sf = *pf;
-    form_cut(sf, text_form.s.h + (2 * MARGIN), DIMENSION_HEIGHT, EDGE_U);
-}
-
-#include "str_val.h"
-
-static void update(ui_element_t * el)
+static inline void update_title(ui_element_t * el)
 {
     __widget_titled_screen_ctx_t * ctx = (__widget_titled_screen_ctx_t *)el->ctx;
     __widget_titled_screen_cfg_t * cfg = (__widget_titled_screen_cfg_t *)el->ui_node->cfg;
 
-    unsigned screen_idx = ctx->current_idx;
-
-    ui_element_t * item = ui_tree_add(el, &cfg->screen_list[screen_idx], 0);
     color_scheme_t cs;
     cs.fg = cfg->color_text;
     if (ctx->focus) {
@@ -60,10 +36,23 @@ static void update(ui_element_t * el)
         cs.bg = cfg->color_bg_unselected;
     }
 
-    create_title(cfg->titles[screen_idx], &cs, &el->f, &item->f);
-    ui_tree_element_draw(item);
+    char * title = cfg->titles[ctx->current_idx];
+
+    form_fill(&ctx->text_box, cs.bg);
+    lcd_text_color_print(title, &ctx->text_form.p, &text_cfg, &cs, 0, 0, 0);
 }
 
+static void update_child(ui_element_t * el)
+{
+    __widget_titled_screen_ctx_t * ctx = (__widget_titled_screen_ctx_t *)el->ctx;
+    __widget_titled_screen_cfg_t * cfg = (__widget_titled_screen_cfg_t *)el->ui_node->cfg;
+
+    ui_element_t * item = ui_tree_add(el, &cfg->screen_list[ctx->current_idx], 0);
+    item->f = el->f;
+    form_cut(&item->f, ctx->text_box.s.h, DIMENSION_HEIGHT, EDGE_U);
+
+    ui_tree_element_draw(item);
+}
 
 static void draw(ui_element_t * el)
 {
@@ -73,7 +62,17 @@ static void draw(ui_element_t * el)
     ctx->current_idx = 0;
     ctx->focus = 0;
 
-    update(el);
+    // форма для текста
+    lcd_text_calc_size(&ctx->text_form.s, &text_cfg);
+
+    // форма всего заголовка
+    ctx->text_box = el->f;
+    form_cut(&ctx->text_box, ctx->text_form.s.h + (2 * MARGIN), DIMENSION_HEIGHT, EDGE_D);
+
+    form_align(&ctx->text_box, &ctx->text_form, &ALIGN_MODE(LI, 6, C, 0));
+
+    update_title(el);
+    update_child(el);
 }
 
 static unsigned process(ui_element_t * el, unsigned event)
@@ -91,7 +90,8 @@ static unsigned process(ui_element_t * el, unsigned event)
             if (ctx->current_idx == cfg->screen_num) {
                 ctx->current_idx = 0;
             }
-            update(el);
+            update_title(el);
+            update_child(el);
             return 1;
         }
         if (event == 4) {
@@ -99,7 +99,8 @@ static unsigned process(ui_element_t * el, unsigned event)
                 ctx->current_idx = cfg->screen_num;
             }
             ctx->current_idx--;
-            update(el);
+            update_title(el);
+            update_child(el);
             return 1;
         }
     } else {
@@ -107,7 +108,7 @@ static unsigned process(ui_element_t * el, unsigned event)
     }
     if (event == 10) {
         ctx->focus = !ctx->focus;
-        update(el);
+        update_title(el);
         return 1;
     }
     return 0;
