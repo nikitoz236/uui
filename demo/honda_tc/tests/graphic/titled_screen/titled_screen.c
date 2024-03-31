83379a7181190774e96c74cf72b994d65cc62f24
Nikita Kulikov <nikitoz236@gmail.com>
2024-03-31 15:08:12 +0300

    move all screens to project view

--- demo/honda_tc/tests/graphic/titled_screen/titled_screen.c
+++ demo/honda_tc/tests/graphic/titled_screen/titled_screen.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
 #include "ui_tree.h"
 #include "emu_lcd.h"
-
 #include "tc_events.h"
-#include "widget_test_rect_with_text.h"
-#include "widget_titled_screen.h"
+#include "view.h"
 
-#include "widget_screen_switch.h"
-
-#include "widget_metric_list.h"
-#include "widget_metric_list_item.h"
-#include "metrics_view.h"
-
-void view_process(char key)
+void emu_view_process(char key)
 {
     unsigned event = 0;
     if (key == 'j') {
@@ -26,8 +18,7 @@ void view_process(char key)
     } else if (key == 'l') {
         event = EVENT_BTN_RIGHT;
     }
-
-    ui_tree_process_event(event);
+    view_process(event);
 }
 
 int main()
@@ -43,119 +34,9 @@ int main()
     };
 
     emu_lcd_init(&emu_lcd_cfg);
+    view_screen_on();
 
-    unsigned screen_selector = 0;
-
-    ui_node_desc_t ui = {
-        .widget = &__widget_screen_switch,
-        .cfg = &(__widget_screen_switch_cfg_t){
-            .selector_ptr = &screen_selector,
-            .screens_num = 5,
-            .screens_list = (ui_node_desc_t[]){
-                {
-                    .widget = &__widget_test_rect_with_text,
-                    .cfg = &(__widget_test_rect_with_text_cfg_t){
-                        .text = "Watch screen",
-                        .cs = {
-                            .bg = 0xA04020,
-                            .fg = 0x23fa75
-                        }
-                    }
-                },
-                {
-                    .widget = &__widget_test_rect_with_text,
-                    .cfg = &(__widget_test_rect_with_text_cfg_t){
-                        .text = "Dashboard",
-                        .cs = {
-                            .bg = 0x708090,
-                            .fg = 0xfffa75
-                        }
-                    }
-                },
-                {
-                    .widget = &__widget_titled_screen,
-                    .cfg = &(__widget_titled_screen_cfg_t){
-                        .screen_num = 2,
-                        .titles = (char * []){
-                            "Debug real val list",
-                            "Debug switches list",
-                        },
-                        .color_text = 0,
-                        .color_bg_unselected = 0x678913,
-                        .color_bg_selected = 0xfeea75,
-                        .screen_list = (ui_node_desc_t []){
-                            {
-                                .widget = &__widget_metric_list,
-                                .cfg = &(__widget_metric_list_cfg_t){
-                                    .ui_node = &(ui_node_desc_t){
-                                        .widget = &__widget_metric_list_item,
-                                        .cfg = &(__widget_metric_list_item_cfg_t){
-                                            .color_text = 0xfffa75,
-                                            .color_value = 0x66CFAB,
-                                            .color_raw = 0xff4450,
-                                            .color_unit = 0x00ff00,
-                                            .color_bg = 0
-                                        }
-                                    },
-                                    .borders = { .w = 4, .h = 4 },
-                                    .gaps = { .x = 2, .y = 4 },
-                                    .color_bg = 0,
-                                    .num = METRIC_VAR_ID_NUM
-                                }
-                            },
-                            {
-                                .widget = &__widget_metric_list,
-                                .cfg = &(__widget_metric_list_cfg_t){
-                                    .ui_node = &(ui_node_desc_t){
-                                        .widget = &__widget_metric_list_item,
-                                        .cfg = &(__widget_metric_list_item_cfg_t){
-                                            .color_text = 0xfffa75,
-                                            .color_value = 0x66CFAB,
-                                            .color_raw = 0xaa4450,
-                                            .color_unit = 0x00ff00,
-                                            .color_bg = 0
-                                        }
-                                    },
-                                    .borders = { .w = 4, .h = 4 },
-                                    .gaps = { .x = 2, .y = 4 },
-                                    .color_bg = 0,
-                                    .num = METRIC_VAR_ID_NUM
-                                }
-                            }
-                        }
-                    }
-                },
-                {
-                    .widget = &__widget_test_rect_with_text,
-                    .cfg = &(__widget_test_rect_with_text_cfg_t){
-                        .text = "Dump",
-                        .cs = {
-                            .bg = 0xA04020,
-                            .fg = 0x23fa75
-                        }
-                    }
-                },
-                {
-                    .widget = &__widget_test_rect_with_text,
-                    .cfg = &(__widget_test_rect_with_text_cfg_t){
-                        .text = "Settings",
-                        .cs = {
-                            .bg = 0xA04020,
-                            .fg = 0x23fa75
-                        }
-                    }
-                }
-            }
-        }
-    };
-    uint8_t ui_ctx[1024];
-    ui_tree_init(ui_ctx, 1024, &ui, &emu_lcd_cfg.size);
-
-    ui_tree_draw();
-
-    ui_tree_debug_print_tree();
-
-    emu_lcd_loop(view_process);
+    emu_lcd_loop(emu_view_process);
 
     return 0;
 }
---
+++ demo/honda_tc/view/view.c
@@ -0,0 +1,143 @@
+#include "ui_tree.h"
+#include "tc_events.h"
+#include "widget_test_rect_with_text.h"
+#include "widget_titled_screen.h"
+
+#include "widget_screen_switch.h"
+
+#include "widget_metric_list.h"
+#include "widget_metric_list_item.h"
+#include "metrics_view.h"
+
+
+
+
+unsigned screen_selector = 0;
+
+ui_node_desc_t ui = {
+    .widget = &__widget_screen_switch,
+    .cfg = &(__widget_screen_switch_cfg_t){
+        .selector_ptr = &screen_selector,
+        .screens_num = 5,
+        .screens_list = (ui_node_desc_t[]){
+            {
+                .widget = &__widget_test_rect_with_text,
+                .cfg = &(__widget_test_rect_with_text_cfg_t){
+                    .text = "Watch screen",
+                    .cs = {
+                        .bg = 0xA04020,
+                        .fg = 0x23fa75
+                    }
+                }
+            },
+            {
+                .widget = &__widget_test_rect_with_text,
+                .cfg = &(__widget_test_rect_with_text_cfg_t){
+                    .text = "Dashboard",
+                    .cs = {
+                        .bg = 0x708090,
+                        .fg = 0xfffa75
+                    }
+                }
+            },
+            {
+                .widget = &__widget_titled_screen,
+                .cfg = &(__widget_titled_screen_cfg_t){
+                    .screen_num = 2,
+                    .titles = (char * []){
+                        "Debug real val list",
+                        "Debug switches list",
+                    },
+                    .color_text = 0,
+                    .color_bg_unselected = 0x678913,
+                    .color_bg_selected = 0xfeea75,
+                    .screen_list = (ui_node_desc_t []){
+                        {
+                            .widget = &__widget_metric_list,
+                            .cfg = &(__widget_metric_list_cfg_t){
+                                .ui_node = &(ui_node_desc_t){
+                                    .widget = &__widget_metric_list_item,
+                                    .cfg = &(__widget_metric_list_item_cfg_t){
+                                        .color_text = 0xfffa75,
+                                        .color_value = 0x66CFAB,
+                                        .color_raw = 0xff4450,
+                                        .color_unit = 0x00ff00,
+                                        .color_bg = 0
+                                    }
+                                },
+                                .borders = { .w = 4, .h = 4 },
+                                .gaps = { .x = 2, .y = 4 },
+                                .color_bg = 0,
+                                .num = METRIC_VAR_ID_NUM
+                            }
+                        },
+                        {
+                            .widget = &__widget_metric_list,
+                            .cfg = &(__widget_metric_list_cfg_t){
+                                .ui_node = &(ui_node_desc_t){
+                                    .widget = &__widget_metric_list_item,
+                                    .cfg = &(__widget_metric_list_item_cfg_t){
+                                        .color_text = 0xfffa75,
+                                        .color_value = 0x66CFAB,
+                                        .color_raw = 0xaa4450,
+                                        .color_unit = 0x00ff00,
+                                        .color_bg = 0
+                                    }
+                                },
+                                .borders = { .w = 4, .h = 4 },
+                                .gaps = { .x = 2, .y = 4 },
+                                .color_bg = 0,
+                                .num = METRIC_VAR_ID_NUM
+                            }
+                        }
+                    }
+                }
+            },
+            {
+                .widget = &__widget_test_rect_with_text,
+                .cfg = &(__widget_test_rect_with_text_cfg_t){
+                    .text = "Dump",
+                    .cs = {
+                        .bg = 0xA04020,
+                        .fg = 0x23fa75
+                    }
+                }
+            },
+            {
+                .widget = &__widget_test_rect_with_text,
+                .cfg = &(__widget_test_rect_with_text_cfg_t){
+                    .text = "Settings",
+                    .cs = {
+                        .bg = 0xA04020,
+                        .fg = 0x23fa75
+                    }
+                }
+            }
+        }
+    }
+};
+uint8_t ui_ctx[1024];
+
+xy_t lcd_size = {
+    .w = 320,
+    .h = 240
+};
+
+
+void view_process(unsigned int event)
+{
+    ui_tree_process_event(event);
+}
+
+void view_screen_on(void)
+{
+    // lcd on
+    ui_tree_init(ui_ctx, 1024, &ui, &lcd_size);
+    ui_tree_draw();
+    // ui_tree_debug_print_tree();
+}
+
+void view_screen_off(void);
+
+void view_idle_screen(void);
+void view_restore_screen(void);
