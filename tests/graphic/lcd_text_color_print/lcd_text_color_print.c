25df6b8ec663753c16267951f67c44109e3f44ed
Nikita Kulikov <nikitoz236@gmail.com>
2024-12-22 21:15:15 +0300

    lcd_text_color_print test not use ui_tree

--- tests/graphic/lcd_text_color_print/lcd_text_color_print.c
+++ tests/graphic/lcd_text_color_print/lcd_text_color_print.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 #include <stdint.h>
 
-
 #include "emu_lcd.h"
+#include "emu_graphic.h"
 
-// #include "api_lcd_color.h"
 #include "lcd_text_color.h"
 
 int main()
 {
     printf("test lcd_text_color_print\r\n");
 
-    __widget_emu_lcd_cfg_t emu_lcd_cfg = {
+    emu_lcd_cfg_t lcd_cfg = {
         .size = { .w = 320, .h = 240 },
-        .bg_color = 0x202020,
-        .border = 10,
+        .scale = 3,
         .px_gap = 0,
-        .scale = 3
+        .border = 10,
+        .bg_color = 0x202020
     };
 
-    emu_lcd_init(&emu_lcd_cfg);
+    form_t lcd_form;
+
+    emu_lcd_init(&lcd_cfg, &lcd_form);
+    emu_graphic_init(lcd_form.s.w, lcd_form.s.h);
+    emu_lcd_clear();
 
     lcd_rect(4, 5, 310, 228, 0x1234);
     lcd_rect(34, 25, 45, 28, 0x4567);
@@ -35,10 +38,10 @@ int main()
             .text_size = { .w = 16, .h = 1 }
         },
         &(color_scheme_t){ .bg = 0xABCD, .fg = 0x166D},
-        0, 0, 0 );
+        0, 0, 0
+    );
 
-
-    emu_lcd_loop(0);
+    emu_graphic_loop(0);
 
     return 0;
 }
