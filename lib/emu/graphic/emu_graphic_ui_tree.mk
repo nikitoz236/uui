79c8e391f62dae6324d2546f2c45dfd5c78d58f9
Nikita Kulikov <nikitoz236@gmail.com>
2024-12-23 08:33:01 +0300

    emu_graphic_ui_tree.mk for easy ui tree projects debug

---
+++ lib/emu/graphic/emu_graphic_ui_tree.mk
@@ -0,0 +1,24 @@
+
+# ui_tree.h
+INC += $(LIB)/graphic/ui_tree
+SRC += $(LIB)/graphic/ui_tree/ui_tree.c
+
+# debug_print.h
+INC += $(LIB)/emu
+
+# misc.h
+INC += $(LIB)/common
+
+# fonts.h
+INC += $(LIB)/graphic/fonts
+SRC += $(LIB)/graphic/fonts/*.c
+
+# lcd_text_color.h
+INC += $(LIB)/graphic/lcd_color
+SRC += $(LIB)/graphic/lcd_color/*.c
+
+SRC += $(LIB)/graphic/lcd_text.c
+SRC += $(LIB)/common/str_utils.c
+SRC += $(LIB)/graphic/align_forms.c
+
+include $(LIB)/emu/graphic/emu_graphic.mk
