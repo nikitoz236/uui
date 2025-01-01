3bd5b9a0c390b5df18a966bc41f451a18ca2aee3
Nikita Kulikov <nikitoz236@gmail.com>
2025-01-01 19:42:53 +0300

    widget_template

---
+++ lib/graphic/ui_tree/widget_template.c
@@ -0,0 +1,44 @@
+#include "widget_template.h"
+
+typedef struct {
+    
+} widget_template_ctx_t;
+
+static void calc(ui_element_t * el)
+{
+    widget_template_cfg_t * cfg = (widget_template_cfg_t *)el->ui_node->cfg;
+    widget_template_ctx_t * ctx = (widget_template_ctx_t *)el->ctx;
+}
+
+static void update(ui_element_t * el)
+{
+    widget_template_cfg_t * cfg = (widget_template_cfg_t *)el->ui_node->cfg;
+    widget_template_ctx_t * ctx = (widget_template_ctx_t *)el->ctx;
+}
+
+static void draw(ui_element_t * el)
+{
+    widget_template_cfg_t * cfg = (widget_template_cfg_t *)el->ui_node->cfg;
+    widget_template_ctx_t * ctx = (widget_template_ctx_t *)el->ctx;
+}
+
+static void select(ui_element_t * el, unsigned selected)
+{
+    widget_template_cfg_t * cfg = (widget_template_cfg_t *)el->ui_node->cfg;
+    widget_template_ctx_t * ctx = (widget_template_ctx_t *)el->ctx;
+}
+
+static unsigned process_event(ui_element_t * el, unsigned event)
+{
+    widget_template_cfg_t * cfg = (widget_template_cfg_t *)el->ui_node->cfg;
+    widget_template_ctx_t * ctx = (widget_template_ctx_t *)el->ctx;
+    return 0;
+}
+
+const widget_desc_t __widget_template = {
+    .draw = draw,
+    .calc = calc,
+    .select = select,
+    .process_event = process_event,
+    .ctx_size = sizeof(widget_template_ctx_t)
+};
---
+++ lib/graphic/ui_tree/widget_template.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "ui_tree.h"
+
+/*
+    правила именования
+
+*/
+
+extern const widget_desc_t __widget_template;
+
+typedef struct {
+    
+} widget_template_cfg_t;
