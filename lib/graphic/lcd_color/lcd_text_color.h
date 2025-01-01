f329eb56d93fdbd85efddff39333024e8ebcaaaa
Nikita Kulikov <nikitoz236@gmail.com>
2025-01-01 17:18:30 +0300

    lcd_text_color comment: gaps не учитывают scale

--- lib/graphic/lcd_color/lcd_text_color.h
+++ lib/graphic/lcd_color/lcd_text_color.h
@@ -3,4 +3,5 @@
 #include "color_type.h"
 #include "color_scheme_type.h"
 
+// gaps не учитывают scale
 void lcd_text_color_print(const char * c, xy_t * pos, const lcd_text_cfg_t * cfg, const color_scheme_t * cs, unsigned tx, unsigned ty, unsigned len);
