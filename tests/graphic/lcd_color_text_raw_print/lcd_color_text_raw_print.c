3f8c08358d67464b4038f22da2136363bf8fdebd
Nikita Kulikov <nikitoz236@gmail.com>
2025-01-07 13:45:43 +0300

    test_lcd_color_text_raw_print

---
+++ tests/graphic/lcd_color_text_raw_print/lcd_color_text_raw_print.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#include "emu_lcd.h"
+#include "emu_graphic.h"
+
+#include "lcd_text_color.h"
+
+int main()
+{
+    printf("test lcd_text_color_print\r\n");
+
+    emu_lcd_cfg_t lcd_cfg = {
+        .size = { .w = 320, .h = 240 },
+        .scale = 3,
+        .px_gap = 0,
+        .border = 10,
+        .bg_color = 0x568901
+    };
+
+    form_t lcd_form;
+
+    emu_lcd_init(&lcd_cfg, &lcd_form);
+    emu_graphic_init(lcd_form.s.w, lcd_form.s.h);
+    emu_lcd_clear();
+
+    extern font_t font_5x7;
+
+    lcd_color_text_raw_print(
+        "Hey bitch!",
+        &(lcd_font_cfg_t){
+            .font = &font_5x7,
+        },
+        &(color_scheme_t){
+            .bg = 0x332211,
+            .fg = 0xAABBCC,
+        },
+        &(xy_t){ .x = 10, .y = 20},
+        0,
+        0,
+        0
+    );
+
+    emu_graphic_loop(0);
+
+    return 0;
+}
