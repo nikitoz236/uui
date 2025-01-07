597a709adc5e86a25829cc625fb31e5e7340c0c7
Nikita Kulikov <nikitoz236@gmail.com>
2025-01-07 09:48:25 +0300

    view/tc_colors.h

---
+++ demo/honda_tc/view/tc_colors.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "color_type.h"
+
+enum {
+    TC_COLOR_BG_UNSELECTED,
+    TC_COLOR_FG_UNSELECTED,
+    TC_COLOR_BG_SELECTED,
+    TC_COLOR_FG_SELECTED
+};
+
+extern const lcd_color_t tc_colors[];
