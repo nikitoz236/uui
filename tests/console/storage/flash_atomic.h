b5cc5baafaeaf7f7bf7e99bb3921668253f7c099
Nikita Kulikov <nikitoz236@gmail.com>
2025-01-08 15:32:08 +0300

    tests/console/storage/flash_atomic.h comments

--- tests/console/storage/flash_atomic.h
+++ tests/console/storage/flash_atomic.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <stdint.h>
 
+// файл определяет атомарный размер слова для записи через тип flash_wr_t
+// и размер страницы для атомарного стирания FLASH_ATOMIC_ERASE_SIZE
+
 typedef uint16_t flash_wr_t;
 
 #define FLASH_ATOMIC_ERASE_SIZE     1024
