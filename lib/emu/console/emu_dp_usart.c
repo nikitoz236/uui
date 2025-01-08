34a2411063ebddad3bd435f41cfa27c44b27d098
Nikita Kulikov <nikitoz236@gmail.com>
2025-01-08 18:18:57 +0300

    dp print test use emu_dp_usart

---
+++ lib/emu/console/emu_dp_usart.c
@@ -0,0 +1,8 @@
+#include "stdio.h"
+
+void __debug_usart_tx_data(const char * s, unsigned len)
+{
+    while(len--) {
+        printf("%c", *s++);
+    }
+}
--- tests/console/dp/test_debug_print.c
+++ tests/console/dp/test_debug_print.c
@@ -1,12 +1,5 @@
-#include "stdio.h"
+#include <stdio.h>
 #include "dp.h"
-
-void __debug_usart_tx_data(const char * s, unsigned len)
-{
-    while(len--) {
-        printf("%c", *s++);
-    }
-}
 
 int main()
 {
