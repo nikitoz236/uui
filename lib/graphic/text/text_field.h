4c15baca88334d372ceb9fade810853e5975c75f
Nikita Kulikov <nikitoz236@gmail.com>
2025-01-10 20:00:19 +0300

    text_field.h

---
+++ lib/graphic/text/text_field.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "lcd_text.h"
+#include "val_text.h"
+#include "forms_align.h"
+
+typedef struct {
+    const lcd_font_cfg_t * fcfg;
+    xy_t limit_char;
+    xy_t margin;
+    xy_t padding;       // ???
+    align_t a;
+} text_field_t;
+
+typedef struct {
+    const text_field_t * tfcfg;
+    char * text;
+    xy_t pos;
+    uint16_t len;
+} text_field_label_t;
+
+typedef struct {
+    const text_field_t * tfcfg;
+    const char * (*to_str)(unsigned x);
+    const val_text_t * vt;
+    xy_t pos;
+    uint16_t len;
+} text_field_value_t;
+
+static inline xy_t text_field_size_px(const text_field_t * cfg)
+{
+    return size_add_padding(lcd_text_size_px(cfg->fcfg, cfg->limit_char), cfg->padding);
+}
