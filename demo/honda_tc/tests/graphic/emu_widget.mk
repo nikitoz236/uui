159ab2bb9966aa4a32bd019e6da9a7479c418c40
Nikita Kulikov <nikitoz236@gmail.com>
2025-01-10 14:52:56 +0300

    fix emu widget mk

--- demo/honda_tc/tests/graphic/emu_widget.mk
+++ demo/honda_tc/tests/graphic/emu_widget.mk
@@ -1,17 +1,17 @@
 LIB = $(PROJECT)/../../lib
 
 # emu_tc.h
+# event_list.h
 INC += $(PROJECT)/tests/graphic
 SRC += $(PROJECT)/tests/graphic/emu_tc.c
-
-# tc_events.h
-INC += $(PROJECT)/view
 
 # forms_split.h
 INC += $(LIB)/graphic/forms
 SRC += $(LIB)/graphic/forms/forms_split.c
+SRC += $(LIB)/graphic/forms/forms_align.c
 
 # systick.h
+# rtc.h
 INC += $(LIB)/hw/include/
 SRC += $(LIB)/emu/hw/systick.c
 SRC += $(LIB)/emu/hw/rtc.c
