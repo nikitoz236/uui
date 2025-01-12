707f062b603f657a8e3910981199ea7063a60a65
Nikita Kulikov <nikitoz236@gmail.com>
2025-01-12 12:03:34 +0300

    widget_screen_switch optional title

--- demo/honda_tc/widgets/common/widget_screen_switch.c
+++ demo/honda_tc/widgets/common/widget_screen_switch.c
@@ -1,19 +1,42 @@
 #include "widget_screen_switch.h"
 #include "event_list.h"
 
+#include "forms_align.h"
+#include "forms_split.h"
+#include "draw_color.h"
+#include "lcd_text_color.h"
+
+typedef struct {
+    form_t title_form;
+    uint8_t last_selector;
+} ctx_t;
+
 static void calc(ui_element_t * el)
 {
     __widget_screen_switch_cfg_t * cfg = (__widget_screen_switch_cfg_t *)el->ui_node->cfg;
+    ctx_t * ctx = (ctx_t *)el->ctx;
     unsigned current_screen = *cfg->selector_ptr;
     ui_node_desc_t * node = &cfg->screens_list[current_screen];
     ui_element_t * item = ui_tree_add(el, node, current_screen);
     ui_tree_element_calc(item);
+    el->f.s = item->f.s;
+    if (cfg->title_cfg) {
+        ctx->title_form.s = text_field_size_px(cfg->title_cfg);
+        el->f.s.h += ctx->title_form.s.h;
+    }
 }
 
 static void extend(ui_element_t * el)
 {
     ui_element_t * item = ui_tree_child(el);
+    __widget_screen_switch_cfg_t * cfg = (__widget_screen_switch_cfg_t *)el->ui_node->cfg;
+    ctx_t * ctx = (ctx_t *)el->ctx;
     item->f = el->f;
+    if (cfg->title_cfg) {
+        ctx->title_form.p = el->f.p;
+        ctx->title_form.s.w = el->f.s.w;
+        form_cut(&item->f, DIMENSION_Y, EDGE_U, ctx->title_form.s.h);
+    }
     ui_tree_element_extend(item);
 }
 
@@ -23,6 +46,20 @@ static void draw_new_child(ui_element_t * el)
     calc(el);
     extend(el);
     ui_tree_element_draw(el);
+}
+
+static void draw(ui_element_t * el)
+{
+    __widget_screen_switch_cfg_t * cfg = (__widget_screen_switch_cfg_t *)el->ui_node->cfg;
+    ctx_t * ctx = (ctx_t *)el->ctx;
+    if (cfg->title_cfg) {
+        draw_color_form(&ctx->title_form, 0x76ab23);
+        xy_t title_pos = align_form_pos(&ctx->title_form, text_field_size_px(cfg->title_cfg), cfg->title_cfg->a, cfg->title_cfg->padding);
+        unsigned current_screen = *cfg->selector_ptr;
+        lcd_color_text_raw_print(cfg->titles[current_screen], cfg->title_cfg->fcfg, &(color_scheme_t){ .bg = 0x76ab23, .fg = 0xAE349E }, &title_pos, 0, 0, 0);
+    }
+    ui_element_t * item = ui_tree_child(el);
+    ui_tree_element_draw(item);
 }
 
 static unsigned process(ui_element_t * el, unsigned event)
@@ -65,5 +102,7 @@ static unsigned process(ui_element_t * el, unsigned event)
 widget_desc_t __widget_screen_switch = {
     .calc = calc,
     .extend = extend,
+    .draw = draw,
     .process_event = process,
+    .ctx_size = sizeof(ctx_t)
 };
--- demo/honda_tc/widgets/common/widget_screen_switch.h
+++ demo/honda_tc/widgets/common/widget_screen_switch.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "ui_tree.h"
+#include "text_field.h"
 
 extern widget_desc_t __widget_screen_switch;
 
@@ -7,4 +8,6 @@ typedef struct {
     unsigned screens_num;
     ui_node_desc_t * screens_list;
     uint8_t * selector_ptr;
+    text_field_t * title_cfg;
+    const char ** titles;
 } __widget_screen_switch_cfg_t;
