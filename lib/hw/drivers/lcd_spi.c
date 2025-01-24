7246acd207dbc738677e50f2fd7f80a76171de8b
Nikita Kulikov <nikitoz236@gmail.com>
2025-01-24 18:22:25 +0300

    lcd_spi use new gpio interface

--- lib/hw/drivers/lcd_spi.c
+++ lib/hw/drivers/lcd_spi.c
@@ -5,20 +5,9 @@ const lcd_cfg_t * lcd_hw_cfg;
 void init_lcd_hw(const lcd_cfg_t * cfg)
 {
     lcd_hw_cfg = cfg;
-    if (cfg->dc.port != GPIO_EMPTY) {
-        gpio_set_cfg(&cfg->dc, cfg->spi_slave.cs_pin_cfg);
-        gpio_set_state(&cfg->dc, 0);
-    }
-    if (cfg->rst.port != GPIO_EMPTY) {
-        gpio_set_cfg(&cfg->rst, cfg->spi_slave.cs_pin_cfg);
-        gpio_set_state(&cfg->rst, 0);
-    }
-    if (cfg->spi_slave.cs_pin.port != GPIO_EMPTY) {
-        gpio_set_cfg(&cfg->spi_slave.cs_pin, cfg->spi_slave.cs_pin_cfg);
-        gpio_set_state(&cfg->spi_slave.cs_pin, 1);
-    }
-
-    init_spi(cfg->spi_slave.spi);
+    init_spi_dev(&cfg->spi_dev);
+    init_gpio_list(cfg->ctrl_lines);
+    init_spi(cfg->spi_dev.spi);
     init_pwm(cfg->bl);
 }
 
--- lib/hw/drivers/lcd_spi.h
+++ lib/hw/drivers/lcd_spi.h
@@ -3,16 +3,20 @@
 #include "spi.h"
 #include "pwm.h"
 
+enum {
+    LCD_DC,
+    LCD_RST
+};
+
 struct lcd_cfg {
-    spi_slave_cfg_t spi_slave;
-    gpio_pin_t dc;
-    gpio_pin_t rst;
-    pwm_cfg_t * bl;
+    const pwm_cfg_t * bl;
+    gpio_list_t * ctrl_lines;
+    spi_dev_cfg_t spi_dev;
 };
 
 typedef struct lcd_cfg lcd_cfg_t;
 
 void init_lcd_hw(const lcd_cfg_t * cfg);
-void init_lcd(const lcd_cfg_t * lcd_cfg);
+void init_lcd(const lcd_cfg_t * cfg);
 void lcd_pwr(unsigned val);
 void lcd_bl(unsigned val);
