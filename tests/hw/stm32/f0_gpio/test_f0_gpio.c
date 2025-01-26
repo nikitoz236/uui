a20aed5cef2bd3dddef3a928903c462ee17efea7
Nikita Kulikov <nikitoz236@gmail.com>
2025-01-26 08:12:08 +0300

    test_f0_gpio check pwm - rcc ok!

--- tests/hw/stm32/f0_gpio/test_f0_gpio.c
+++ tests/hw/stm32/f0_gpio/test_f0_gpio.c
@@ -3,6 +3,8 @@
 #include "periph_rcc.h"
 #include "periph_gpio.h"
 
+#include "stm_pwm.h"
+
 const rcc_cfg_t rcc_cfg = {
     .hse_val = 8000000,
     .pll_src = PLL_SRC_PREDIV,
@@ -34,13 +36,36 @@ const gpio_list_t leds = {
     }
 };
 
+const pwm_cfg_t pwm_led = {
+    .ch = 1 - 1,
+    .freq = 1000,
+    .max_val = 10,
+    .pclk = PCLK_TIM1,
+    .tim = TIM1,
+    .gpio = &(gpio_t){
+        .gpio = {
+            .port = GPIO_PORT_A,
+            .pin = 8
+        },
+        .cfg = {
+            .mode = GPIO_MODE_AF,
+            .speed = GPIO_SPEED_HIGH,
+            .type = GPIO_TYPE_PP,
+            .af = 2
+        }
+    }
+};
+
 int main()
 {
     rcc_apply_cfg(&rcc_cfg);
 
     pclk_ctrl(&PCLK_GPIOA, 1);
-    init_gpio_list(&leds);
-    gpio_list_set_state(&leds, 1, 1);
+    // init_gpio_list(&leds);
+    // gpio_list_set_state(&leds, 1, 1);
+
+    init_pwm(&pwm_led);
+    pwm_set_ccr(&pwm_led, 4);
 
     while (1) {};
 
