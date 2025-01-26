b8573b501c95da2b8f4c24df69568f3d1a5727ba
Nikita Kulikov <nikitoz236@gmail.com>
2025-01-26 08:05:52 +0300

    f0_gpio and rcc test

---
+++ tests/hw/stm32/f0_gpio/test_f0_gpio.c
@@ -0,0 +1,48 @@
+#include "periph_header.h"
+#include "periph_pclk.h"
+#include "periph_rcc.h"
+#include "periph_gpio.h"
+
+const rcc_cfg_t rcc_cfg = {
+    .hse_val = 8000000,
+    .pll_src = PLL_SRC_PREDIV,
+    .pll_prediv = 1,
+    .pll_mul = 6,
+    .sysclk_src = SYSCLK_SRC_PLL,
+    .hclk_div = HCLK_DIV1,
+    .apb_div = {
+        APB_DIV1,
+    }
+};
+
+const gpio_list_t leds = {
+    .cfg = {
+        .mode = GPIO_MODE_OUTPUT,
+        .speed = GPIO_SPEED_HIGH,
+        .type = GPIO_TYPE_PP,
+    },
+    .count = 2,
+    .pin_list = (const gpio_pin_t[]){
+        {
+            .port = GPIO_PORT_A,
+            .pin = 8
+        },
+        {
+            .port = GPIO_PORT_A,
+            .pin = 11
+        }
+    }
+};
+
+int main()
+{
+    rcc_apply_cfg(&rcc_cfg);
+
+    pclk_ctrl(&PCLK_GPIOA, 1);
+    init_gpio_list(&leds);
+    gpio_list_set_state(&leds, 1, 1);
+
+    while (1) {};
+
+    return 0;
+}
