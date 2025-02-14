2169937716861f01b2d3d2fe829b41215e35a420
Nikita Kulikov <nikitoz236@gmail.com>
2025-02-14 10:13:01 +0300

    ili9341 hw test fix makefile and config - f1 spi driver name

--- tests/hw/stm32/ili9341/config.h
+++ tests/hw/stm32/ili9341/config.h
@@ -3,7 +3,8 @@
 #include "periph_gpio.h"
 #include "periph_pclk.h"
 #include "stm_pwm.h"
-#include "stm32f10x_spi.h"
+#include "periph_spi.h"
+
 #include "lcd_spi.h"
 
 extern const rcc_cfg_t rcc_cfg;
