970dd74693a8c2d33efacbb3d4f19c70891c8263
Nikita Kulikov <nikitoz236@gmail.com>
2025-02-16 09:51:10 +0300

    dlc_hw_emu recieve kline data

--- demo/honda_tc/tests/hw/dlc_hw_emu/dlc_hw_emu.c
+++ demo/honda_tc/tests/hw/dlc_hw_emu/dlc_hw_emu.c
@@ -7,6 +7,7 @@
 #include "stm_pwm.h"
 #include "stm_usart.h"
 
+#include "mstimer.h"
 #include "delay_blocking.h"
 #include "dp.h"
 
@@ -86,10 +87,85 @@ const gpio_list_t buttons = {
     }
 };
 
+const usart_cfg_t kline_usart;
+
+void kline_usart_irq_handler(void)
+{
+    usart_irq_handler(&kline_usart);
+}
+
+typedef union {
+    struct {
+        uint8_t byte;
+        uint8_t flag;
+    };
+    uint16_t word;
+} atomic_byte_flag_t;
+atomic_byte_flag_t krx;
+
+void kline_rx_byte_handler(char byte);
+
+const usart_cfg_t kline_usart = {
+    .usart = USART2,
+    .default_baud = 9600,
+    .rx_pin = &(const gpio_t) {
+        .gpio = {
+            .port = GPIO_PORT_A,
+            .pin = 3
+        },
+        .cfg = {
+            .mode = GPIO_MODE_AF,
+            .pull = GPIO_PULL_UP,
+            .af = 1,
+        }
+    },
+    .tx_pin = &(const gpio_t) {
+        .gpio = {
+            .port = GPIO_PORT_A,
+            .pin = 2
+        },
+        .cfg = {
+            .mode = GPIO_MODE_AF,
+            .speed = GPIO_SPEED_HIGH,
+            .type = GPIO_TYPE_OD,
+            .af = 1,
+        }
+    },
+    .tx_dma = {
+        .dma_ch = 4,
+    },
+    // .rx_dma = {
+    //     .dma_ch = 3,
+    //     .size = 0,
+    // },
+    .pclk = PCLK_USART2,
+    .irqn = USART2_IRQn,
+    .usart_irq_handler = kline_usart_irq_handler,
+    .rx_byte_handler = kline_rx_byte_handler
+};
+
 void __debug_usart_tx_data(const char * s, unsigned len)
 {
     usart_tx(&debug_usart, s, len);
 }
+
+
+
+uint8_t kline_rx_buf[32];
+unsigned kline_rx_idx = 0;
+
+mstimer_t kline_timeout = { .timeout = 50 };
+
+void kline_rx_byte_handler(char byte)
+{
+    if(kline_rx_idx < 32) {
+        kline_rx_buf[kline_rx_idx++] = byte;
+    }
+    mstimer_reset(&kline_timeout);
+    dp("<- ");
+    dpx(byte, 1);
+    dn();
+};
 
 int main()
 {
@@ -113,10 +189,24 @@ int main()
     pwm_set_ccr(&buz_cfg, 0);
 
     usart_set_cfg(&debug_usart);
+    usart_set_cfg(&kline_usart);
 
     dpn("Honda DLC emulator");
 
-    while (1) {};
+    while (1) {
+        if (kline_rx_idx) {
+            if (mstimer_is_over(&kline_timeout)) {
+                kline_rx_idx = 0;
+                dpn("timeout");
+            }
+        }
+        if (kline_rx_idx == 5) {
+            dp("req : ");
+            dpxd(kline_rx_buf, 1, 5);
+            dn();
+            kline_rx_idx = 0;
+        }
+    };
 
     return 0;
 }
