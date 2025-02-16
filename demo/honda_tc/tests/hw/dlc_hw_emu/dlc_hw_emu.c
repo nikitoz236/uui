733371ecbaa243f5343f90b61a6e5626c1e72faf
Nikita Kulikov <nikitoz236@gmail.com>
2025-02-16 12:54:16 +0300

    dlc_hw_emu dlc_parse work and engine control via r s for run and stop

--- demo/honda_tc/tests/hw/dlc_hw_emu/dlc_hw_emu.c
+++ demo/honda_tc/tests/hw/dlc_hw_emu/dlc_hw_emu.c
@@ -43,6 +43,23 @@ const pwm_cfg_t buz_cfg = {
     }
 };
 
+
+const usart_cfg_t kline_usart;
+const usart_cfg_t debug_usart;
+
+void kline_usart_irq_handler(void)
+{
+    usart_irq_handler(&kline_usart);
+}
+
+void debug_usart_irq_handler(void)
+{
+    usart_irq_handler(&debug_usart);
+}
+
+void kline_rx_byte_handler(char byte);
+void debug_rx_byte_handler(char byte);
+
 const usart_cfg_t debug_usart = {
     .usart = USART1,
     .default_baud = 115200,
@@ -69,41 +86,14 @@ const usart_cfg_t debug_usart = {
             .af = 1
         }
     },
-    .pclk = PCLK_USART1,
-    .irqn = USART1_IRQn
-};
-
-const gpio_list_t buttons = {
-    .count = 4,
-    .cfg = {
-        .mode = GPIO_MODE_INPUT,
-        .pull = GPIO_PULL_UP,
+    .tx_dma = {
+        .dma_ch = 2,
     },
-    .pin_list = (gpio_pin_t []){
-        { .port = GPIO_PORT_B, .pin = 0 },      // LU
-        { .port = GPIO_PORT_B, .pin = 1 },      // LD
-        { .port = GPIO_PORT_B, .pin = 3 },      // RU
-        { .port = GPIO_PORT_B, .pin = 4 },      // RD
-    }
+    .pclk = PCLK_USART1,
+    .irqn = USART1_IRQn,
+    .usart_irq_handler = debug_usart_irq_handler,
+    .rx_byte_handler = debug_rx_byte_handler
 };
-
-const usart_cfg_t kline_usart;
-
-void kline_usart_irq_handler(void)
-{
-    usart_irq_handler(&kline_usart);
-}
-
-typedef union {
-    struct {
-        uint8_t byte;
-        uint8_t flag;
-    };
-    uint16_t word;
-} atomic_byte_flag_t;
-atomic_byte_flag_t krx;
-
-void kline_rx_byte_handler(char byte);
 
 const usart_cfg_t kline_usart = {
     .usart = USART2,
@@ -144,14 +134,31 @@ const usart_cfg_t kline_usart = {
     .rx_byte_handler = kline_rx_byte_handler
 };
 
+const gpio_list_t buttons = {
+    .count = 4,
+    .cfg = {
+        .mode = GPIO_MODE_INPUT,
+        .pull = GPIO_PULL_UP,
+    },
+    .pin_list = (gpio_pin_t []){
+        { .port = GPIO_PORT_B, .pin = 0 },      // LU
+        { .port = GPIO_PORT_B, .pin = 1 },      // LD
+        { .port = GPIO_PORT_B, .pin = 3 },      // RU
+        { .port = GPIO_PORT_B, .pin = 4 },      // RD
+    }
+};
+
 void __debug_usart_tx_data(const char * s, unsigned len)
 {
     usart_tx(&debug_usart, s, len);
 }
 
+unsigned engine_state = 0;
+unsigned engine_last = 0;
 
 
 uint8_t kline_rx_buf[32];
+uint8_t kline_tx_buf[32];
 unsigned kline_rx_idx = 0;
 
 mstimer_t kline_timeout = { .timeout = 50 };
@@ -162,10 +169,27 @@ void kline_rx_byte_handler(char byte)
         kline_rx_buf[kline_rx_idx++] = byte;
     }
     mstimer_reset(&kline_timeout);
-    dp("<- ");
-    dpx(byte, 1);
-    dn();
+    // dp("< ");
+    // dpx(byte, 1);
 };
+
+
+void debug_rx_byte_handler(char byte)
+{
+    // dp("DBG : ");
+    // dpl(&byte, 1);
+    // dn();
+
+    if (byte == 'r') {
+        engine_state = 1;
+    }
+
+    if (byte == 's') {
+        engine_state = 0;
+    }
+}
+
+unsigned check_kline_req(uint8_t * data, unsigned len, uint8_t * resp);
 
 int main()
 {
@@ -174,13 +198,18 @@ int main()
     pclk_ctrl(&(pclk_t)PCLK_GPIOA, 1);
     pclk_ctrl(&(pclk_t)PCLK_GPIOB, 1);
 
-    // init_gpio(&pwr_ctrl);
-    // gpio_set_state(&pwr_ctrl, 0);
+    pclk_ctrl(&(pclk_t)PCLK_DMA, 1);
+
+    init_gpio_list(&buttons);
+
+    usart_set_cfg(&debug_usart);
+    usart_set_cfg(&kline_usart);
 
     init_systick();
     __enable_irq();
 
     init_pwm(&buz_cfg);
+
     pwm_set_freq(&buz_cfg, 700);
     pwm_set_ccr(&buz_cfg, 10);
     delay_ms(100);
@@ -188,22 +217,41 @@ int main()
     delay_ms(100);
     pwm_set_ccr(&buz_cfg, 0);
 
-    usart_set_cfg(&debug_usart);
-    usart_set_cfg(&kline_usart);
-
     dpn("Honda DLC emulator");
 
+    unsigned echo_len = 0;
+
     while (1) {
+        if (engine_last != engine_state) {
+            engine_last = engine_state;
+            if (engine_state) {
+                dpn("Ignition ON");
+            } else {
+                dpn("Ignition OFF");
+            }
+        }
+
         if (kline_rx_idx) {
             if (mstimer_is_over(&kline_timeout)) {
                 kline_rx_idx = 0;
+                echo_len = 0;
                 dpn("timeout");
             }
         }
-        if (kline_rx_idx == 5) {
+        if (kline_rx_idx == (echo_len + 5)) {
             dp("req : ");
-            dpxd(kline_rx_buf, 1, 5);
+            dpxd(&kline_rx_buf[echo_len], 1, 5);
             dn();
+            if (engine_state) {
+                unsigned tx_len =check_kline_req(&kline_rx_buf[echo_len], 5, kline_tx_buf);
+                if (tx_len) {
+                    dp("resp: ");
+                    dpxd(kline_tx_buf, 1, tx_len);
+                    dn();
+                    usart_tx(&kline_usart, kline_tx_buf, tx_len);
+                }
+                echo_len = tx_len;
+            }
             kline_rx_idx = 0;
         }
     };
