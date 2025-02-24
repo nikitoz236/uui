509bdae623d26bf505c42dc8426db339f75c45e5
Nikita Kulikov <nikitoz236@gmail.com>
2025-02-24 18:23:21 +0300

    COLOR macro for colors convert

--- lib/hw/drivers/color_type.h
+++ lib/hw/drivers/color_type.h
@@ -2,3 +2,8 @@
 #include <stdint.h>
 
 typedef uint16_t lcd_color_t;
+
+#define __convertColor(r, g, b)	((((r) & 0xF8) << 8) + (((g) & 0xFC) << 3) + (((b) & 0xF8) >> 3))
+#define __convertColorHEX(v)		(__convertColor((v >> 16), (v >> 8), (v)))
+
+#define COLOR(x)    __convertColorHEX(x)
