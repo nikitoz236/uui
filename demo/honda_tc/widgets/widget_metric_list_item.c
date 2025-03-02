1e4b87ffeeb7bd290e088112ebdee4fa88b6ba01
Nikita Kulikov <nikitoz236@gmail.com>
2025-03-02 12:17:13 +0300

    widget_metric_list_item dec formated and raw print

--- demo/honda_tc/widgets/widget_metric_list_item.c
+++ demo/honda_tc/widgets/widget_metric_list_item.c
@@ -3,11 +3,27 @@
 #include "forms_align.h"
 #include "lcd_text_color.h"
 #include "text_field.h"
+#include "text_label_color.h"
+#include "array_size.h"
+
 #include "str_val_buf.h"
+#include "str_val.h"
+
+const char * raw_hex(unsigned x)
+{
+    char * str = str_val_buf_get();
+    hex_to_str(&x, str, 2);
+    return str;
+}
+
+struct uv {
+    int val;
+    unsigned raw;
+};
 
 typedef struct {
     xy_t pos;
-    int val;
+    struct uv uv;
 } ctx_t;
 
 extern const font_t font_5x7;
@@ -16,46 +32,12 @@ static const text_field_t tf = {
     .fcfg = &(lcd_font_cfg_t){
         .font = &font_5x7,
     },
-    .limit_char = { .x = TEXT_LEN("metric name      12345678 units 0000") },
+    .limit_char = { .x = 50 },
     .padding = { .x = 2, .y = 2 },
     .a = ALIGN_LIC,
 };
 
-enum {
-    ITEM_NAME,
-    ITEM_REAL,
-    ITEM_RAW,
-    ITEM_UNIT,
-    ITEM_BOOL = ITEM_REAL,
-};
-
-#define VAL_LEN 8
-
-static const char * metric_str_raw_val(unsigned idx)
-{
-    char * str = str_val_buf_get();
-    unsigned val = metric_var_get_raw(idx);
-    hex_to_str(&val, str, 2);
-    return str;
-}
-
-static const char * metric_str_real_val(unsigned idx)
-{
-    char * str = str_val_buf_get();
-    int val = metric_var_get_real(idx);
-    val_text_t vt = {
-        .f = metric_var_get_factor(idx),
-        .p = 2,
-        .s = 1,
-        .zl = 0,
-        .zr = 0,
-        .t = DEC,
-        .vs = VAL_SIZE_32,
-        .l = VAL_LEN
-    };
-    val_text_to_str(str, &val, &vt);
-    return str;
-}
+const lcd_color_t bg[] = { 0x111111, 0x113222 };
 
 static const char * metric_str_bool(unsigned idx)
 {
@@ -66,32 +48,50 @@ static const char * metric_str_bool(unsigned idx)
     return "inactive";
 }
 
-color_scheme_t cst = {
-    .fg = 0xfffa75,
-    .bg = 0
+
+//     [ITEM_RAW]   = { .cs = &csv, .tv = { .tfcfg = &tf, .to_str = metric_str_raw_val,    .len = 4,                   .pos = { .x = METRIC_NAME_MAX_LEN + 1 + VAL_LEN + 1 + METRIC_UNIT_MAX_LEN + 1 } } },
+//     [ITEM_REAL]  = { .cs = &csv, .tv = { .tfcfg = &tf, .to_str = metric_str_real_val,   .len = 8,                   .pos = { .x = METRIC_NAME_MAX_LEN + 1 } } },
+
+//     [ITEM_BOOL]  = { .cs = &csv, .tv = { .tfcfg = &tf, .to_str = metric_str_bool,       .len = TEXT_LEN("inactive"),.pos = { .x = METRIC_NAME_MAX_LEN + 1 } } },
+
+/*
+=================
+0                 18            32        42
+metric name       -12345678901  units     0000
+=======================================
+
+*/
+
+static const struct lscolor label_static_real[] = {
+    { .color = 0xABCDEF, .l = { .to_str = metric_var_get_name, .xy = { .x = 0 }, } },
+    { .color = 0xABCDEF, .l = { .to_str = metric_var_get_unit, .xy = { .x = 32 }, } },
 };
 
-color_scheme_t csv = {
-    .fg = 0xff2a35,
-    .bg = 0
+static const struct lscolor label_static_bool = {
+      .color = 0xABCDEF, .l = { .to_str = metric_bool_get_name, .xy = { .x = 0 }, }
 };
 
-typedef struct {
-    text_field_value_t tv;
-    color_scheme_t * cs;
-} item_tf_t;
-
-static const item_tf_t items_real[] = {
-    [ITEM_NAME]  = { .cs = &cst, .tv = { .tfcfg = &tf, .to_str = metric_var_get_name,   .len = METRIC_NAME_MAX_LEN, .pos = { .x = 0 }, } },
-    [ITEM_UNIT]  = { .cs = &cst, .tv = { .tfcfg = &tf, .to_str = metric_var_get_unit,   .len = METRIC_UNIT_MAX_LEN, .pos = { .x = METRIC_NAME_MAX_LEN + 1 + VAL_LEN + 1 }, } },
-    [ITEM_RAW]   = { .cs = &csv, .tv = { .tfcfg = &tf, .to_str = metric_str_raw_val,    .len = 4,                   .pos = { .x = METRIC_NAME_MAX_LEN + 1 + VAL_LEN + 1 + METRIC_UNIT_MAX_LEN + 1 } } },
-    [ITEM_REAL]  = { .cs = &csv, .tv = { .tfcfg = &tf, .to_str = metric_str_real_val,   .len = 8,                   .pos = { .x = METRIC_NAME_MAX_LEN + 1 } } },
+void metric_vt(unsigned idx, val_text_t * vt)
+{
+    unsigned point = 0;
+    dec_factor_t factor = 0;
+    metric_var_get_factor_point(idx, &factor, &point);
+    vt->f = factor;
+    vt->p = point;
+    vt->zr = 0;
+    vt->zl = 0;
 };
 
-static const item_tf_t items_bool[] = {
-    [ITEM_NAME]  = { .cs = &cst, .tv = { .tfcfg = &tf, .to_str = metric_bool_get_name,  .len = METRIC_NAME_MAX_LEN, .pos = { .x = 0 }, } },
-    [ITEM_BOOL]  = { .cs = &csv, .tv = { .tfcfg = &tf, .to_str = metric_str_bool,       .len = TEXT_LEN("inactive"),.pos = { .x = METRIC_NAME_MAX_LEN + 1 } } },
+static const struct lvcolor label_val[] = {
+    { .color = 0xABCDEF, .l = { .len = 10, .rep = { .s = 1, .vs = VAL_SIZE_32 }, .t = LVFI, .vt_by_idx = metric_vt, .ofs = offsetof(struct uv, val), .xy = { .x = 18 }, } },
+    { .color = 0xABCDEF, .l = { .len = 4,  .rep = { .vs = VAL_SIZE_32 }, .t = LF, .to_str = raw_hex, .ofs = offsetof(struct uv, raw), .xy = { .x = 42 }, } },
 };
+
+void update_uv(struct uv * uv, unsigned idx)
+{
+    uv->val = metric_var_get_real(idx);
+    uv->raw = metric_ecu_get_raw(idx);
+}
 
 static void calc(ui_element_t * el)
 {
@@ -102,59 +102,89 @@ static void extend(ui_element_t * el)
 {
     ctx_t * ctx = (ctx_t *)el->ctx;
     ctx->pos = align_form_pos(&el->f, lcd_text_size_px(tf.fcfg, tf.limit_char), tf.a, tf.padding);
-    // printf("widget_metric_list_item extend item %d size %d %d, text pos %d %d\n", el->idx, el->f.s.w, el->f.s.h, ctx->pos.x, ctx->pos.y);
 }
 
-void update_tf(const item_tf_t * item, xy_t pos, unsigned idx)
-{
-    const char * str = item->tv.to_str(idx);
-    lcd_color_text_raw_print(str, item->tv.tfcfg->fcfg, item->cs, &pos, &item->tv.tfcfg->limit_char, &item->tv.pos, item->tv.len);
-}
+/*
+    итак
+
+    у нас есть здесь просто текстовое поле, в котором как минимум есть заголовок, который зависит от индекса
+        для значений в нем печатаются
+            само значение которое точно получается в контекст из индекса
+                но печатать его непонятно как, у каждого свое представление, знак, фактор, количество символов после запятой итд, тоже по индексу становится понятно
+            единица измерения которая зависит от индекса
+            сырое значение из контекста
+        для булевых значений 
+
+*/
 
 static void update_real(ui_element_t * el)
 {
-    ctx_t * ctx = (ctx_t *)el->ctx;
-    int real = metric_var_get_real(el->idx);
-    if (ctx->val != real) {
-        ctx->val = real;
-        update_tf(&items_real[ITEM_REAL], ctx->pos, el->idx);
-        update_tf(&items_real[ITEM_RAW], ctx->pos, el->idx);
+    struct uv uv = {};
+    update_uv(&uv, el->idx);
+
+    for (unsigned i = 0; i < ARRAY_SIZE(label_val); i++) {
+        // label_value_print(&label_val[i].l, &uv, el->idx);
     }
 }
 
 static void update_bool(ui_element_t * el)
 {
-    ctx_t * ctx = (ctx_t *)el->ctx;
-    int val = metric_bool_get_val(el->idx);
-    if (ctx->val != val) {
-        ctx->val = val;
-        update_tf(&items_bool[ITEM_NAME], ctx->pos, el->idx);
-        update_tf(&items_bool[ITEM_BOOL], ctx->pos, el->idx);
-    }
+
 }
 
 static void draw_real(ui_element_t * el)
 {
     ctx_t * ctx = (ctx_t *)el->ctx;
-    ctx->val = metric_ecu_get_real(el->idx);
-    update_tf(&items_real[ITEM_NAME], ctx->pos, el->idx);
-    update_tf(&items_real[ITEM_UNIT], ctx->pos, el->idx);
-    update_tf(&items_real[ITEM_REAL], ctx->pos, el->idx);
-    update_tf(&items_real[ITEM_RAW], ctx->pos, el->idx);
+
+    color_scheme_t cs = {
+        .bg = bg[el->active],
+    };
+
+    tf_ctx_t tf_ctx = {
+        .tfcfg = &tf,
+        .xy = ctx->pos,
+    };
+
+    for (unsigned i = 0; i < ARRAY_SIZE(label_static_real); i++) {
+        cs.fg = label_static_real[i].color;
+        label_static_print(&tf_ctx, &label_static_real[i].l, &cs, el->idx);
+    }
+
+    update_uv(&ctx->uv, el->idx);
+
+    for (unsigned i = 0; i < ARRAY_SIZE(label_val); i++) {
+        label_value_print(&tf_ctx, &label_val[i].l, &cs, 0, &ctx->uv, el->idx);
+    }
+
+    // update_tf(&items_real[ITEM_NAME], ctx->pos, el->idx);
+    // update_tf(&items_real[ITEM_UNIT], ctx->pos, el->idx);
+    // update_tf(&items_real[ITEM_REAL], ctx->pos, el->idx);
+    // update_tf(&items_real[ITEM_RAW], ctx->pos, el->idx);
 }
 
 static void draw_bool(ui_element_t * el)
 {
     ctx_t * ctx = (ctx_t *)el->ctx;
-    ctx->val = metric_ecu_get_bool(el->idx);
-    update_tf(&items_bool[ITEM_NAME], ctx->pos, el->idx);
-    update_tf(&items_bool[ITEM_BOOL], ctx->pos, el->idx);
+
+    color_scheme_t cs = {
+        .bg = bg[el->active],
+    };
+
+    tf_ctx_t tf_ctx = {
+        .tfcfg = &tf,
+        .xy = ctx->pos,
+    };
+
+    cs.fg = label_static_bool.color;
+    label_static_print(&tf_ctx, &label_static_bool.l, &cs, el->idx);
+
+    // ctx->val = metric_ecu_get_bool(el->idx);
 }
 
 const widget_desc_t __widget_metric_list_item_real = {
     .calc = calc,
     .draw = draw_real,
-    .update = update_real,
+    // .update = update_real,
     .extend = extend,
     .ctx_size = sizeof(ctx_t)
 };
@@ -162,7 +192,7 @@ const widget_desc_t __widget_metric_list_item_real = {
 const widget_desc_t __widget_metric_list_item_bool = {
     .calc = calc,
     .draw = draw_bool,
-    .update = update_bool,
+    // .update = update_bool,
     .extend = extend,
     .ctx_size = sizeof(ctx_t)
 };
