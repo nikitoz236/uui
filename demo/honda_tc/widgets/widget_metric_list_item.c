ed24b5194efa6a09791acc7068cb17175084fbea
Nikita Kulikov <nikitoz236@gmail.com>
2025-03-02 17:12:36 +0300

    metrics_list use lable lists

--- demo/honda_tc/widgets/widget_metric_list_item.c
+++ demo/honda_tc/widgets/widget_metric_list_item.c
@@ -8,13 +8,7 @@
 
 #include "str_val_buf.h"
 #include "str_val.h"
-
-const char * raw_hex(unsigned x)
-{
-    char * str = str_val_buf_get();
-    hex_to_str(&x, str, 2);
-    return str;
-}
+#include "draw_color.h"
 
 typedef struct {
     int val;
@@ -49,27 +43,12 @@ static const char * metric_str_bool(unsigned idx)
 }
 
 
-//     [ITEM_RAW]   = { .cs = &csv, .tv = { .tfcfg = &tf, .to_str = metric_str_raw_val,    .len = 4,                   .pos = { .x = METRIC_NAME_MAX_LEN + 1 + VAL_LEN + 1 + METRIC_UNIT_MAX_LEN + 1 } } },
-//     [ITEM_REAL]  = { .cs = &csv, .tv = { .tfcfg = &tf, .to_str = metric_str_real_val,   .len = 8,                   .pos = { .x = METRIC_NAME_MAX_LEN + 1 } } },
-
-//     [ITEM_BOOL]  = { .cs = &csv, .tv = { .tfcfg = &tf, .to_str = metric_str_bool,       .len = TEXT_LEN("inactive"),.pos = { .x = METRIC_NAME_MAX_LEN + 1 } } },
-
-/*
-=================
-0                 18            32        42
-metric name       -12345678901  units     0000
-=======================================
-
-*/
-
-static const struct lvcolor label_static_real[] = {
-    { .color = 0xABCDEF, .l = { .to_str = metric_var_get_name, .t = LP_T_FIDX, .xy = { .x = 0 }, } },
-    { .color = 0xABCDEF, .l = { .to_str = metric_var_get_unit, .t = LP_T_FIDX, .xy = { .x = 32 }, } },
-};
-
-static const struct lvcolor label_static_bool = {
-      .color = 0xABCDEF, .l = { .to_str = metric_bool_get_name, .t = LP_T_FIDX, .xy = { .x = 0 }, }
-};
+const char * raw_hex(unsigned x)
+{
+    char * str = str_val_buf_get();
+    hex_to_str(&x, str, 2);
+    return str;
+}
 
 void metric_vt(val_text_t * vt, unsigned idx)
 {
@@ -82,9 +61,26 @@ void metric_vt(val_text_t * vt, unsigned idx)
     vt->zl = 0;
 };
 
-static const struct lvcolor label_val[] = {
-    { .color = 0xABCDEF, .l = { .len = 10, .rep = { .s = 1, .vs = VAL_SIZE_32 }, .t = LVFI, .vt_by_idx = metric_vt, .ofs = offsetof(uv_t, val), .xy = { .x = 18 }, } },
-    { .color = 0xABCDEF, .l = { .len = 4,  .rep = { .vs = VAL_SIZE_32 }, .t = LF, .to_str = raw_hex, .ofs = offsetof(uv_t, raw), .xy = { .x = 42 }, } },
+/*
+=================
+0                 18            32        42
+metric name       -12345678901  units     0000
+=======================================
+
+*/
+
+static const lp_color_t label_static_real[] = {
+    { .color = 0x524D3F, .l = { .to_str = metric_var_get_name, .t = LP_T_FIDX, .xy = { .x = 0 }, } },
+    { .color = 0x1D5F90, .l = { .to_str = metric_var_get_unit, .t = LP_T_FIDX, .xy = { .x = 32 }, } },
+};
+
+static const lp_color_t label_static_bool = {
+      .color = 0x1D5F90, .l = { .to_str = metric_bool_get_name, .t = LP_T_FIDX, .xy = { .x = 0 }, }
+};
+
+static const lp_color_t label_val[] = {
+    { .color = 0x52AD6F, .l = { .len = 10, .rep = { .s = 1, .vs = VAL_SIZE_32 }, .t = LVFI, .vt_by_idx = metric_vt, .ofs = offsetof(uv_t, val), .xy = { .x = 18 }, } },
+    { .color = 0xAB4422, .l = { .len = 4,  .rep = { .vs = VAL_SIZE_32 }, .t = LF, .to_str = raw_hex, .ofs = offsetof(uv_t, raw), .xy = { .x = 42 }, } },
 };
 
 void update_uv(uv_t * uv, unsigned idx)
@@ -92,6 +88,9 @@ void update_uv(uv_t * uv, unsigned idx)
     uv->val = metric_var_get_real(idx);
     uv->raw = metric_ecu_get_raw(idx);
 }
+
+const label_list_t ll_static_real = { .count = ARRAY_SIZE(label_static_real), .wrap_list = label_static_real };
+const label_list_t ll_val_real = { .count = ARRAY_SIZE(label_val), .wrap_list = label_val, .ctx_update = (void(*)(void * ctx, unsigned x))update_uv };
 
 static void calc(ui_element_t * el)
 {
@@ -121,22 +120,13 @@ static void update_real(ui_element_t * el)
 {
     ctx_t * ctx = (ctx_t *)el->ctx;
 
-    color_scheme_t cs = {
-        .bg = bg[el->active],
-    };
-
     tf_ctx_t tf_ctx = {
         .tfcfg = &tf,
         .xy = ctx->pos,
     };
 
     uv_t uv = {};
-    update_uv(&uv, el->idx);
-
-    for (unsigned i = 0; i < ARRAY_SIZE(label_val); i++) {
-        cs.fg = label_static_real[i].color;
-        lp(&tf_ctx, &label_val[i].l, &cs, 0, &ctx->uv, el->idx);
-    }
+    lp_color(&tf_ctx, bg[el->active], &ll_val_real, el->idx, &uv, &ctx->uv);
 }
 
 static void update_bool(ui_element_t * el)
@@ -148,25 +138,13 @@ static void draw_real(ui_element_t * el)
 {
     ctx_t * ctx = (ctx_t *)el->ctx;
 
-    color_scheme_t cs = {
-        .bg = bg[el->active],
-    };
-
     tf_ctx_t tf_ctx = {
         .tfcfg = &tf,
         .xy = ctx->pos,
     };
-
-    for (unsigned i = 0; i < ARRAY_SIZE(label_static_real); i++) {
-        cs.fg = label_static_real[i].color;
-        lp(&tf_ctx, &label_static_real[i].l, &cs, 0, 0, el->idx);
-    }
-
-    update_uv(&ctx->uv, el->idx);
-
-    for (unsigned i = 0; i < ARRAY_SIZE(label_val); i++) {
-        lp(&tf_ctx, &label_val[i].l, &cs, 0, &ctx->uv, el->idx);
-    }
+    draw_color_form(&el->f, bg[el->active]);
+    lp_color(&tf_ctx, bg[el->active], &ll_static_real, el->idx, 0, 0);
+    lp_color(&tf_ctx, bg[el->active], &ll_val_real, el->idx, &ctx->uv, 0);
 }
 
 static void draw_bool(ui_element_t * el)
