392cd1232cdd6d637aa9419d313269fadb684766
Nikita Kulikov <nikitoz236@gmail.com>
2025-03-03 20:10:14 +0300

    test_screen_switch_title

---
+++ demo/honda_tc/tests/graphic/screen_switch_title/test_screen_switch_title.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include "emu_tc.h"
+
+#include "widget_screen_switch.h"
+#include "event_list.h"
+
+#include "draw_color.h"
+
+extern font_t font_5x7;
+uint8_t screen_selector = 0;
+
+void widget_color_rect_draw(ui_element_t * el)
+{
+    lcd_color_t * color = el->ui_node->cfg;
+    printf("    rect color hex %06x\n", *color);
+    draw_color_form(&el->f, *color);
+}
+
+const widget_desc_t widget_color_rect = {
+    .draw = widget_color_rect_draw
+};
+
+const lcd_color_t color_list[] = {
+    0xAABBCC,
+    0x11BBCC,
+    0xAA22CC,
+    0xAABB33,
+};
+
+ui_node_desc_t ui = {
+    .widget = &__widget_screen_switch_titled,
+    .cfg = &(__widget_screen_switch_cfg_t){
+        .selector_ptr = &screen_selector,
+        .screens_num = 4,
+        .title_cfg = &(text_field_t){
+            .fcfg = &(lcd_font_cfg_t){
+                .font = &font_5x7,
+                .gaps = { .x = 2, .y = 2 },
+                .scale = 2
+            },
+            .limit_char = { .y = 1 },
+            .a = ALIGN_LIC,
+            .padding = { .x = 2, .y = 2 }
+        },
+        .titles = (const char *[]){
+            "Widget",
+            "Debug Metrics Bool",
+            "Debug Metrics REAL",
+            "DUMP ECU", 
+        },
+        .screens_list = (ui_node_desc_t[]){
+            {
+                .widget = &widget_color_rect,
+                .cfg = &color_list[0]
+            },
+            {
+                .widget = &widget_color_rect,
+                .cfg = &color_list[1]
+            },
+            {
+                .widget = &widget_color_rect,
+                .cfg = &color_list[2]
+            },
+            {
+                .widget = &widget_color_rect,
+                .cfg = &color_list[3]
+            },
+        }
+    }
+};
+
+
+int main()
+{
+    printf("test __widget_screen_switch_titled\r\n");
+
+
+    emu_ui_node(&ui);
+    return 0;
+}
