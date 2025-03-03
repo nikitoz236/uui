ea3d178b3c9d35c806a30d256d18a9d94fed4881
Nikita Kulikov <nikitoz236@gmail.com>
2025-03-03 20:34:47 +0300

    val_mod max comment

--- lib/common/val_mod.h
+++ lib/common/val_mod.h
@@ -6,5 +6,7 @@ typedef enum {
     MOD_OP_SUB,
 } val_mod_op_t;
 
+// непонятно что такое min max, пока что это крайние значения, которые может принимать переменная
+
 unsigned val_mod_unsigned(void * ptr, val_size_t size, val_mod_op_t op, unsigned ovf, unsigned min, unsigned max, unsigned step);
 unsigned val_mod_signed(void * ptr, val_size_t size, val_mod_op_t op, unsigned ovf, int min, int max, int step);
