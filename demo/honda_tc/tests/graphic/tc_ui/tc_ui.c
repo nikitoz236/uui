3f9d7f3dd9363e7ef57eb3382caca53a0bb20490
Nikita Kulikov <nikitoz236@gmail.com>
2025-03-03 08:11:58 +0300

    tc_ui - dump

---
+++ demo/honda_tc/tests/graphic/tc_ui/tc_ui.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "emu_tc.h"
+#include "dlc_poll.h"
+
+#include "widget_tc_dump.h"
+#include "widget_screen_switch.h"
+
+uint8_t selector = 0;
+
+ui_node_desc_t ui = {
+    .widget = &__widget_screen_switch,
+    .cfg = &(__widget_screen_switch_cfg_t){
+        .screens_num = 1,
+        .screens_list = (ui_node_desc_t[]){
+            {
+                .widget = &__widget_tc_dump
+            },
+        },
+        .selector_ptr = &selector,
+    }
+};
+
+int main()
+{
+    printf("test tc ui\r\n");
+    emu_ui_node(&ui);
+    return 0;
+}
