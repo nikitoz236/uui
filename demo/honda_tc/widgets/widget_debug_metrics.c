f422e75d8c15b26ef1f9c79ce97c3ba13d4ef1fe
Nikita Kulikov <nikitoz236@gmail.com>
2025-03-04 08:51:19 +0300

    tc_ui - widget_screen_debug_metrics

--- demo/honda_tc/widgets/widget_debug_metrics.c
+++ demo/honda_tc/widgets/widget_debug_metrics.c
@@ -1,71 +1,105 @@
 #include "ui_tree.h"
 
-#include "widget_titled_screen.h"
+#include "text_label_color.h"
+#include "lcd_color.h"
+#include "draw_color.h"
+#include "val_mod.h"
+
+#include "event_list.h"
+
+#include "widget_debug_metrics.h"
 #include "widget_metric_list.h"
 #include "widget_metric_list_item.h"
-#include "metrics_view.h"
 
-ui_node_desc_t debug_metrics_screen = {
-    .widget = &__widget_titled_screen,
-    .cfg = &(__widget_titled_screen_cfg_t){
-        .screen_num = 2,
-        .titles = (const char * []){
-            "Debug real val list",
-            "Debug switches list",
-        },
-        .color_text = 0,
-        .color_bg_unselected = 0x678913,
-        .color_bg_selected = 0xfeea75,
-        .screen_list = (ui_node_desc_t []){
-            {
-                .widget = &__widget_metric_list,
-                .cfg = &(__widget_metric_list_cfg_t){
-                    .ui_node = &(ui_node_desc_t){
-                        .widget = &__widget_metric_list_item,
-                        .cfg = &(__widget_metric_list_item_cfg_t){
-                            .color_text = 0xfffa75,
-                            .color_value = 0x66CFAB,
-                            .color_raw = 0xff4450,
-                            .color_unit = 0x00ff00,
-                            .color_bg = 0
-                        }
-                    },
-                    .borders = { .w = 4, .h = 4 },
-                    .gaps = { .x = 2, .y = 4 },
-                    .color_bg = 0,
-                    .num = METRIC_VAR_ID_NUM
-                }
-            },
-            {
-                .widget = &__widget_metric_list,
-                .cfg = &(__widget_metric_list_cfg_t){
-                    .ui_node = &(ui_node_desc_t){
-                        .widget = &__widget_metric_list_item,
-                        .cfg = &(__widget_metric_list_item_cfg_t){
-                            .color_text = 0xfffa75,
-                            .color_value = 0x66CFAB,
-                            .color_raw = 0xaa4450,
-                            .color_unit = 0x00ff00,
-                            .color_bg = 0
-                        }
-                    },
-                    .borders = { .w = 4, .h = 4 },
-                    .gaps = { .x = 2, .y = 4 },
-                    .color_bg = 0,
-                    .num = METRIC_VAR_ID_NUM
-                }
-            }
-        }
-    }
+// #include "widget_titled_screen.h"
+// #include "metrics_view.h"
+
+typedef struct {
+    form_t title_form;
+    tf_ctx_t title_ctx;
+    unsigned selector;
+} ctx_t;
+
+
+/*
+============================================
+0 2         13
+< METRICS   >
+
+============================================
+
+*/
+
+const lp_color_t title = {
+    .color = 0xCE4512, .l = { .xy = { .x = 2 }, .len = 10, .text = (const char * []){ "METRICS", "DLC DUMP" }, .t = LP_T_LIDX }
 };
+
+const lp_color_t local_title = {
+    .color = 0xCE4512, .l = { .xy = { .x = 19 }, .len = 10, .text = (const char * []){ "REAL", "BOOL" }, .t = LP_T_LIDX }
+};
+
+const label_list_t title_selector = {
+    .wrap_list = (lp_color_t []) {
+        { .color = 0xCE4512, .l = { .xy = { .x =  0 }, .len = 1, .text_list = (const char *[]){ "<", 0 }, .t = LP_T_LIDX } },
+        { .color = 0xCE4512, .l = { .xy = { .x = 13 }, .len = 1, .text_list = (const char *[]){ ">", 0 }, .t = LP_T_LIDX } },
+        { .color = 0xCE4512, .l = { .xy = { .x = 17 }, .len = 1, .text_list = (const char *[]){ 0, "<" }, .t = LP_T_LIDX } },
+        { .color = 0xCE4512, .l = { .xy = { .x = 25 }, .len = 1, .text_list = (const char *[]){ 0, ">" }, .t = LP_T_LIDX } },
+    },
+    .count = 4
+};
+
+// const lp_color_t type = {
+//     .color = 0xCE4512, .l = { .xy = { .x = 1 }, .len = 20, .text_list = { "REAL", "BOOL" }, .t = LP_T }
+// }
+
+static const lcd_color_t bg = 0x4585E1;
+
+static void redraw(ui_element_t * el)
+{
+    ctx_t * ctx = (ctx_t *)el->ctx;
+    widget_screen_debug_metrics_cfg_t * cfg = (widget_screen_debug_metrics_cfg_t *)el->ui_node->cfg;
+
+    lp(&ctx->title_ctx, &local_title.l, &(color_scheme_t){ .fg = title.color, .bg = bg }, 0, 0, ctx->selector);
+    lp_color(&ctx->title_ctx, bg, &title_selector, el->active, 0, 0);
+}
 
 static void draw(ui_element_t * el)
 {
-    ui_element_t * item = ui_tree_add(el, &debug_metrics_screen, 0);
-    item->f = el->f;
-    ui_tree_element_draw(item);
+    ctx_t * ctx = (ctx_t *)el->ctx;
+    widget_screen_debug_metrics_cfg_t * cfg = (widget_screen_debug_metrics_cfg_t *)el->ui_node->cfg;
+    ctx->selector = 0;
+    ctx->title_form = el->f;
+    tf_ctx_calc(&ctx->title_ctx, &ctx->title_form, cfg->title_cfg);
+    draw_color_form(&ctx->title_form, bg);
+
+    lp(&ctx->title_ctx, &title.l, &(color_scheme_t){ .fg = title.color, .bg = bg }, 0, 0, el->idx);
+
+    // ui_element_t * item = ui_tree_add(el, &debug_metrics_screen, 0);
+    // ui_tree_element_draw(item);
+    redraw(el);
 }
 
-widget_desc_t __widget_screen_debug_metrics = {
-    .draw = draw
+static unsigned process(ui_element_t * el, unsigned event)
+{
+    ctx_t * ctx = (ctx_t *)el->ctx;
+    widget_screen_debug_metrics_cfg_t * cfg = (widget_screen_debug_metrics_cfg_t *)el->ui_node->cfg;
+
+    if (event == EVENT_BTN_DOWN) {
+        val_mod_unsigned(&ctx->selector, VAL_SIZE_32, MOD_OP_ADD, 1, 0, 1, 1);
+        redraw(el);
+        return 1;
+    }
+    if (event == EVENT_BTN_UP) {
+        val_mod_unsigned(&ctx->selector, VAL_SIZE_32, MOD_OP_SUB, 1, 0, 1, 1);
+        redraw(el);
+        return 1;
+    }
+    return 0;
+}
+
+const widget_desc_t widget_screen_debug_metrics = {
+    .draw = draw,
+    .select = redraw,
+    .process_event = process,
+    .ctx_size = sizeof(ctx_t)
 };
--- demo/honda_tc/widgets/widget_debug_metrics.h
+++ demo/honda_tc/widgets/widget_debug_metrics.h
@@ -1,3 +1,8 @@
+#pragma once
 #include "ui_tree.h"
 
-extern widget_desc_t __widget_screen_debug_metrics;
+extern const widget_desc_t widget_screen_debug_metrics;
+
+typedef struct {
+    text_field_t * title_cfg;
+} widget_screen_debug_metrics_cfg_t;
