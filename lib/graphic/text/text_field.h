22e962679e72513140841df7d27e05569f59b582
Nikita Kulikov <nikitoz236@gmail.com>
2025-03-05 09:58:58 +0300

    text_field tf_ctx_calc fix align non limit tf

--- lib/graphic/text/text_field.h
+++ lib/graphic/text/text_field.h
@@ -46,47 +46,53 @@ static inline unsigned tf_ctx_calc(tf_ctx_t * ctx, form_t * f, const text_field_
     }
 
     for (unsigned d = 0; d < DIMENSION_COUNT; d++) {
-        ctx->xy.ca[d] = f->p.ca[d];
-
-        unsigned text_linear_size = 0;
-        unsigned char_linear_size = cfg->fcfg->font->size.ca[d];
         unsigned gap = cfg->fcfg->gaps.ca[d];
         if (gap == 0) {
             gap = 1;
             // охуенный план также расширять gaps и scale если там нули
             // и у тебя упрется по одной из координат всегда текст, и другую координату можно будет уменьшить
         }
+        unsigned char_linear_size = cfg->fcfg->font->size.ca[d];
+
+        unsigned text_len = 0;
+        unsigned text_linear_size = 0;
+
+        unsigned form_size_px = f->s.ca[d];
+        unsigned available_px = form_size_px - (2 * cfg->padding.ca[d]);
+        unsigned available_chars = (available_px + gap) / ((char_linear_size * scale) + gap);
+
         if (cfg->limit_char.ca[d]) {
             // вариант когда мы уменьшаем размер формы
-            unsigned text_len = cfg->limit_char.ca[d];
-            ctx->size.ca[d] = text_len;
-            text_linear_size = (char_linear_size * text_len * scale) + ((text_len - 1) * gap);
-
-            unsigned new_size = text_linear_size + (2 * cfg->padding.ca[d]);
-            if (new_size > f->s.ca[d]) {
+            text_len = cfg->limit_char.ca[d];
+            if (text_len > available_chars) {
                 // если нужно больше места то возвращаем 0, чтобы list понял что форма не влезла
                 return 0;
             }
-            // перезаписываем размер по данной координате в форме
-            f->s.ca[d] = new_size;
-
-            // align
-            if (cfg->a.ca[d].center) {
-                ctx->xy.ca[d] += (f->s.ca[d] - text_linear_size) / 2;
-            } else {
-                if (cfg->a.ca[d].edge == EDGE_L) {
-                    ctx->xy.ca[d] += cfg->padding.ca[d];
-                } else {
-                    ctx->xy.ca[d] += f->s.ca[d] - cfg->padding.ca[d] - text_linear_size;
-                }
-            }
         } else {
             // вариант когда мы расширяем по максимуму
-            unsigned available = f->s.ca[d] - (2 * cfg->padding.ca[d]);
-            ctx->size.ca[d] = (available + gap) / ((char_linear_size * scale) + gap);
+            text_len = available_chars;
+        }
 
-            // align
-            ctx->xy.ca[d] += cfg->padding.ca[d];
+        ctx->size.ca[d] = text_len;
+        text_linear_size = (char_linear_size * text_len * scale) + ((text_len - 1) * gap);
+
+        // перезаписываем размер по данной координате в форме
+        // - надо ли ? основной вопрос
+        if (cfg->limit_char.ca[d]) {
+            form_size_px = text_linear_size + (2 * cfg->padding.ca[d]);
+            f->s.ca[d] = form_size_px;
+        }
+
+        // align
+        ctx->xy.ca[d] = f->p.ca[d];
+        if (cfg->a.ca[d].center) {
+            ctx->xy.ca[d] += (f->s.ca[d] - text_linear_size) / 2;
+        } else {
+            if (cfg->a.ca[d].edge == EDGE_L) {
+                ctx->xy.ca[d] += cfg->padding.ca[d];
+            } else {
+                ctx->xy.ca[d] += f->s.ca[d] - cfg->padding.ca[d] - text_linear_size;
+            }
         }
     }
 
