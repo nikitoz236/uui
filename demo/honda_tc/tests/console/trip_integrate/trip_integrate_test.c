523b286370808a4dd658d46e2019a3180106a21e
Nikita Kulikov <nikitoz236@gmail.com>
2025-03-14 19:54:58 +0300

    trip_integrate_test print dist cons

--- demo/honda_tc/tests/console/trip_integrate/trip_integrate_test.c
+++ demo/honda_tc/tests/console/trip_integrate/trip_integrate_test.c
@@ -15,9 +15,10 @@ int main()
 
     for (int i = 0; i < 10; i++) {
         trip_integrate(3400 * 4, 123, 4100 / 4);
-        printf("dist %d, fuel %d, cons h %d, period %d\n",
+        printf("dist %d, fuel %d, cons l/100 %d, cons h %d, period %d\n",
             trip_get_dist_m(),
             trip_get_fuel_ml(),
+            trip_integrate_get_real(__INTEGRATE_VAR_CONS_MOMENT_DIST),
             trip_integrate_get_real(__INTEGRATE_VAR_CONS_MOMENT_TIME),
             trip_integrate_get_real(__INTEGRATE_VAR_INTEGRATE_PERIOD)
         );
