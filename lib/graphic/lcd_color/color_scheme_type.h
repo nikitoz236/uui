0c643b0a714c42d27a5413feb64dba2db8f9939b
Nikita Kulikov <nikitoz236@gmail.com>
2025-03-16 09:46:32 +0300

    color_scheme_inverted

--- lib/graphic/lcd_color/color_scheme_type.h
+++ lib/graphic/lcd_color/color_scheme_type.h
@@ -6,8 +6,7 @@ typedef struct __attribute__((packed)) {
     lcd_color_t bg;
 } color_scheme_t;
 
-static inline void invert_color_scheme(color_scheme_t * cs, color_scheme_t * inv)
+static inline color_scheme_t color_scheme_inverted(color_scheme_t * cs)
 {
-    inv->fg = cs->bg;
-    inv->bg = cs->fg;
+    return (color_scheme_t) { .fg = cs->bg, .bg = cs->fg };
 }
