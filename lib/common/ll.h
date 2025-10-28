e5394f0278d62d0fc39c3dfcd2d4f1ddfa03f2de
Nikita Kulikov <nikitoz236@gmail.com>
2025-10-28 12:25:40 +0300

    ll add comments

--- lib/common/ll.h
+++ lib/common/ll.h
@@ -14,6 +14,11 @@ struct ll_item {
     uint8_t data[];
 };
 
+// head - указатель на переменную с указателем на первый элемент списка
+
 void ll_add(ll_item_t ** head, ll_item_t * item);
 void ll_remove(ll_item_t ** head, ll_item_t * item);
+
+// для каждого элемента списка вызывает функцию process
+// если функция вернула ненулевое значение, элемент удаляется
 void ll_process(ll_item_t ** head, unsigned(*process)(void *));
