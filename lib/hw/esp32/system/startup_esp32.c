6125a6b7235812841e6fd96a41ee2b834ceacf12
Nikita Kulikov <nikitoz236@gmail.com>
2025-11-29 12:52:44 +0300

    startup_esp3 wdt comments align

--- lib/hw/esp32/system/startup_esp32.c
+++ lib/hw/esp32/system/startup_esp32.c
@@ -8,18 +8,18 @@ int main(void);
 void disable_wdt(void)
 {
     // Отключаем системный WDT TG0
-    TIMERG0.wdtwprotect.val = 0x50D83AA1;   // Разблокировка
-    TIMERG0.wdtfeed.val = 1;                // Сброс счётчика
-    TIMERG0.wdtconfig0.val = 0;          // Отключаем WDT
-    TIMERG0.wdtwprotect.val = 0;               // Блокируем
+    TIMERG0.wdtwprotect.val = 0x50D83AA1;       // Разблокировка
+    TIMERG0.wdtfeed.val = 1;                    // Сброс счётчика
+    TIMERG0.wdtconfig0.val = 0;                 // Отключаем WDT
+    TIMERG0.wdtwprotect.val = 0;                // Блокируем
 
+    // Отключаем системный WDT RTC
+    RTCCNTL.wdt_wprotect = 0x50D83AA1;          // Разблокировка
+    RTCCNTL.wdt_config0.val = 0;                // Отключаем WDT
+    RTCCNTL.ext_xtl_conf.xtal32k_wdt_en = 0;    // Отключаем внешний кристалл
+    RTCCNTL.wdt_wprotect = 0;                   // Блокируем
 
-    RTCCNTL.wdt_wprotect = 0x50D83AA1;   // Разблокировка
-    RTCCNTL.wdt_config0.val = 0;         // Отключаем WDT
-    RTCCNTL.ext_xtl_conf.xtal32k_wdt_en = 0;            // Отключаем внешний кристалл
-    RTCCNTL.wdt_wprotect = 0;               // Блокируем
-
-    RTCCNTL.swd_conf.swd_disable = 1;         // Отключаем Super WDT
+    RTCCNTL.swd_conf.swd_disable = 1;           // Отключаем Super WDT
 }
 
 void _reset(void)
