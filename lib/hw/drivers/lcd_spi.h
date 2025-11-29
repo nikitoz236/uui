6a8b97f4e2278e338b4a75101550f00c2f1fa0fa
Nikita Kulikov <nikitoz236@gmail.com>
2025-11-29 12:27:14 +0300

    lcd_cfg contain glass cfg

--- lib/hw/drivers/lcd_spi.h
+++ lib/hw/drivers/lcd_spi.h
@@ -10,12 +10,21 @@ enum {
     LCD_DC,
 };
 
+struct lcd_glass_cfg {
+    uint16_t width;
+    uint16_t height;
+    uint16_t x_offset;
+    uint16_t y_offset;
+    uint8_t x_flip : 1;
+    uint8_t y_flip : 1;
+    uint8_t xy_swap : 1;
+};
+
 struct lcd_cfg {
     pwm_cfg_t * bl;
     gpio_list_t * ctrl_lines;
     spi_dev_cfg_t spi_dev;
-    uint16_t x_offset;
-    uint16_t y_offset;
+    struct lcd_glass_cfg gcfg;
 };
 
 typedef const struct lcd_cfg lcd_cfg_t;
