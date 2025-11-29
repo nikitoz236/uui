b7e287e6ed9712fdfca98434285086bd47200b4b
Nikita Kulikov <nikitoz236@gmail.com>
2025-11-29 14:29:19 +0300

    color_type.h rgb 565 comment

--- lib/hw/drivers/color_type.h
+++ lib/hw/drivers/color_type.h
@@ -3,7 +3,9 @@
 
 typedef uint16_t lcd_color_t;
 
-#define __convertColor(r, g, b)	((((r) & 0xF8) << 8) + (((g) & 0xFC) << 3) + (((b) & 0xF8) >> 3))
-#define __convertColorHEX(v)		(__convertColor((v >> 16), (v >> 8), (v)))
+// BGR disabled, used RGB565 as u16
+// 0b rrrrrggg gggbbbbb
+#define __convertColor(r, g, b)     ((((r) & 0xF8) << 8) + (((g) & 0xFC) << 3) + (((b) & 0xF8) >> 3))
+#define __convertColorHEX(v)        (__convertColor((v >> 16), (v >> 8), (v)))
 
 #define COLOR(x)    __convertColorHEX(x)
