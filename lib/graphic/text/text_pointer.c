b424b102afe64bb419d96b8d92ec5c4daa1764bb
Nikita Kulikov <nikitoz236@gmail.com>
2026-01-01 12:24:36 +0300

    text pointer

---
+++ lib/graphic/text/text_pointer.c
@@ -0,0 +1,92 @@
+
+#include "text_pointer.h"
+
+unsigned text_ptr_next_str(tptr_t * tptr)
+{
+    if (tptr->tlim.y) {
+        if (tptr->cpos.y >= (tptr->tlim.y - 1)) {
+            return 0;
+        }
+    }
+
+    tptr->cpos.x = 0;
+    tptr->cpos.y++;
+
+    tptr->cxy.x =tptr->fxy.x;
+    tptr->cxy.y += tptr->cstep.y;
+
+    return 1;
+}
+
+unsigned text_ptr_next_char(tptr_t * tptr)
+{
+    if (tptr->tlim.x) {
+        if (tptr->cpos.x >= (tptr->tlim.x - 1)) {
+            return text_ptr_next_str(tptr);
+        }
+    }
+
+    tptr->cpos.x++;
+    tptr->cxy.x += tptr->cstep.x;
+
+    return 1;
+}
+
+void text_ptr_init(tptr_t * tptr, const lcd_font_cfg_t * fcfg, xy_t pos_px, const xy_t * limit_chars)
+{
+    if (!tptr) {
+        return;
+    }
+
+    tptr->fcfg = fcfg;
+
+    if (fcfg) {
+        unsigned scale = fcfg->scale;
+        if (scale == 0) {
+            scale = 1;
+        }
+
+        for (unsigned d = 0; d < DIMENSION_COUNT; d++) {
+            unsigned gap = fcfg->gaps.ca[d];
+            if (gap == 0) {
+                gap = 1;
+            }
+            tptr->cstep.ca[d] = (fcfg->font->size.ca[d] * scale) + gap;
+        }
+    } else {
+        tptr->cstep.x = 0;
+        tptr->cstep.y = 0;
+    }
+
+    if (limit_chars) {
+        tptr->tlim = *limit_chars;
+    } else {
+        tptr->tlim.x = 0;
+        tptr->tlim.y = 0;
+    }
+
+    // не понимаю зачем раньше передавал положение черех указатель? чтобы сослаться на поле какойто формы?
+    tptr->fxy = pos_px;
+    tptr->cxy = pos_px;
+}
+
+unsigned text_ptr_set_char_pos(tptr_t * tptr, xy_t pos)
+{
+    for (unsigned d = 0; d < DIMENSION_COUNT; d++) {
+        int shift = pos.ca[d];
+
+        if (shift < 0) {
+            if (tptr->tlim.ca[d]) {
+                shift += tptr->tlim.ca[d]; //  - ( -1 * shift )
+            } else {
+                shift = 0;
+            }
+        } else {
+            if (shift >= tptr->tlim.ca[d]) {
+                return 0;
+            }
+        }
+        tptr->cpos.ca[d] = shift;
+        tptr->cxy.ca[d] = tptr->fxy.ca[d] + (shift * tptr->cstep.ca[d]);
+    }
+}
---
+++ lib/graphic/text/text_pointer.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "lcd_text.h"
+
+/*
+    Итак я смотрю на функцию :
+
+            // основная функция, позволяет расположить строку str в координатах pos
+            // cfg - конфигурация шрифта, при этом если cfg.scale = 0, то он считается равным 1, если cfg.gaps = 0, то они считаются равными 1
+            //      на cfg.scale домножается font, cfg.gaps в пикселях определяет расстояние между символами, не домножается на cfg.scale
+            // cs - цветовая схема, fg - цвет текста, bg - цвет фона
+            // limit_chars - ограничение размер текста в символах, если 0 - то просто до безконечности вправо рисует (ну для коротких текстов можно использовать)
+            //      можно указать только limit_chars.x, тогда будет печататься одна строка (text_size.y = 1)
+            // pos_chars - положение текста в текстовом поле в символах, если 0 - то в начало,
+            //      также может быть отрицательным, для выравнивания слева, но нужен limit_chars. при len != 0 считает считает правый край текста. иначе левый
+            // len - длина строки, если 0 - то до конца текста или до text_size.x
+            void lcd_color_text_raw_print(const char * str, const lcd_font_cfg_t * cfg, const color_scheme_t * cs, const xy_t * pos_px, const xy_t * limit_chars, const xy_t * pos_chars, unsigned len);
+
+    понимаю что большинство проблем здесь математика вычисления положения знакоместа
+*/
+
+
+typedef struct {
+    const lcd_font_cfg_t * fcfg;
+    xy_t fxy;       // положение поля в пикселях
+    xy_t tlim;      // размеры поля в символах
+    xy_t cpos;      // положение курсора в символах
+
+    xy_t cxy;       // положение курсора в пикселях
+    xy_t cstep;     // вектор сдвига курсора на 1 символ / строку
+} tptr_t;
+
+void text_ptr_init(tptr_t * tptr, const lcd_font_cfg_t * fcfg, xy_t pos_px, const xy_t * limit_chars);
+
+unsigned text_ptr_next_char(tptr_t * tptr);
+unsigned text_ptr_next_str(tptr_t * tptr);
+unsigned text_ptr_set_char_pos(tptr_t * tptr, xy_t pos);
