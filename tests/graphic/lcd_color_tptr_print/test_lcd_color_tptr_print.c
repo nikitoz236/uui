774866532270bc7fdcc3624a99b0379d8201fb89
Nikita Kulikov <nikitoz236@gmail.com>
2026-01-01 12:20:43 +0300

    test_lcd_color_tptr_print draw some str

--- tests/graphic/lcd_color_tptr_print/test_lcd_color_tptr_print.c
+++ tests/graphic/lcd_color_tptr_print/test_lcd_color_tptr_print.c
@@ -24,11 +24,32 @@ int main()
     emu_graphic_init_xy(lcd_form.s);
     emu_lcd_clear();
 
-    lcd_rect(4, 5, 200, 98, 0xA234);
+    lcd_rect(4, 5, 200, 98, 0x1234);
 
 
+    extern const font_t font_5x7;
 
+    lcd_font_cfg_t fcfg = {
+        .font = &font_5x7,
+        .gaps = 0,
+        .scale = 2
+    };
 
+    color_scheme_t cs = {.bg = 0x1234, .fg = 0xA234};
+
+    tptr_t tp;
+    text_ptr_init(&tp, &fcfg, (xy_t){8, 9}, &(xy_t){20, 8});
+
+    text_ptr_set_char_pos(&tp, (xy_t){ 3, 4 });
+    lcd_color_tptr_print(&tp, "lol", cs, 0);
+
+    text_ptr_next_str(&tp);
+    lcd_color_tptr_print(&tp, "kek", cs, 0);
+
+    text_ptr_set_char_pos(&tp, (xy_t){ 6, 2 });
+    lcd_color_tptr_print(&tp, "cheburek", cs, 0);
+
+    lcd_color_tptr_print(&tp, "aga", cs, 0);
 
     emu_graphic_loop(0);
 
