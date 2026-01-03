f72aa51c2b1ff0ce7bee058a4e1c2e2d827e97ed
Nikita Kulikov <nikitoz236@gmail.com>
2026-01-03 10:51:25 +0300

    esp32 i2c rework clock calc

--- lib/hw/esp32/periph/esp32_i2c.c
+++ lib/hw/esp32/periph/esp32_i2c.c
@@ -20,60 +20,26 @@ typedef union {
 } i2c_cmd_t;
 
 
-void calc_bus_clk(void)
+static void calc_bus_clk(i2c_dev_t * dev, unsigned freq)
 {
-    uint32_t source_clk = 40000000;
-    uint32_t bus_freq = 400000;
+    const uint32_t source_clk = 40000000;
+    uint32_t clkm_div = source_clk / (freq * 1024) + 1;     // = 1
+    uint32_t sclk_freq = source_clk / clkm_div;             // = 40 000 000
+    uint32_t half_cycle = sclk_freq / freq / 2;             // = 50
 
-    i2c_dev_t * hw = _cfg->dev;
-
-    struct clk_vals {
-        uint16_t clkm_div;          /*!< I2C core clock divider */
-        uint16_t scl_low;           /*!< I2C scl low period */
-        uint16_t scl_high;          /*!< I2C scl high period */
-        uint16_t scl_wait_high;     /*!< I2C scl wait_high period */
-        uint16_t sda_hold;          /*!< I2C scl low period */
-        uint16_t sda_sample;        /*!< I2C sda sample time */
-        uint16_t setup;             /*!< I2C start and stop condition setup period */
-        uint16_t hold;              /*!< I2C start and stop condition hold period  */
-        uint16_t tout;              /*!< I2C bus timeout period */
-    } clk_vals;
-
-    struct clk_vals * clk_cal = &clk_vals;
-
-    uint32_t clkm_div = source_clk / (bus_freq * 1024) + 1;     // = 1
-    uint32_t sclk_freq = source_clk / clkm_div;                 // = 40 000 000
-    uint32_t half_cycle = sclk_freq / bus_freq / 2;             // = 50
-
-    // dp("clkm_div : "); dpd(clkm_div, 5); dn();
-
-    //SCL
-    clk_cal->clkm_div = clkm_div;
-    clk_cal->scl_low = half_cycle;
     // default, scl_wait_high < scl_high
     // Make 80KHz as a boundary here, because when working at lower frequency, too much scl_wait_high will faster the frequency
     // according to some hardware behaviors.
 
-    clk_cal->scl_wait_high = (bus_freq >= 80 * 1000) ? (half_cycle / 2 - 2) : (half_cycle / 4);
-    clk_cal->scl_high = half_cycle - clk_cal->scl_wait_high;
-    clk_cal->sda_hold = half_cycle / 4;
-    clk_cal->sda_sample = half_cycle / 2;
-    clk_cal->setup = half_cycle;
-    clk_cal->hold = half_cycle;
+    uint32_t scl_wait_high = (freq >= 80 * 1000) ? (half_cycle / 2 - 2) : (half_cycle / 4);
+    uint32_t scl_high = half_cycle - scl_wait_high;
+    uint32_t sda_sample = half_cycle / 2;
 
-    //default we set the timeout value to about 10 bus cycles
-    // log(20*half_cycle)/log(2) = log(half_cycle)/log(2) +  log(20)/log(2)
-    clk_cal->tout = (int)(sizeof(half_cycle) * 8 - __builtin_clz(5 * half_cycle)) + 2;
+    // ASSERT((scl_wait_high < sda_sample) && (sda_sample < scl_high));
 
-    /* Verify the assumptions made by the hardware */
+    uint32_t sda_hold = half_cycle / 4;
 
-    // HAL_ASSERT(clk_cal->scl_wait_high < clk_cal->sda_sample &&
-    //            clk_cal->sda_sample < clk_cal->scl_high);
-
-    struct clk_vals * bus_cfg = &clk_vals;
-
-
-    // HAL_FORCE_MODIFY_U32_REG_FIELD(hw->clk_conf, sclk_div_num, );
+    //SCL
 
     /* According to the Technical Reference Manual, the following timings must be subtracted by 1.
      * However, according to the practical measurement and some hardware behaviour, if wait_high_period and scl_high minus one.
@@ -82,24 +48,30 @@ void calc_bus_clk(void)
      * to some extent. */
 
     i2c_clk_conf_reg_t tmp;
-    tmp.val = hw->clk_conf.val;
-    tmp.sclk_div_num = bus_cfg->clkm_div - 1;
-    hw->clk_conf.val = tmp.val;
+    tmp.val = dev->clk_conf.val;
+    tmp.sclk_div_num = clkm_div - 1;
+    dev->clk_conf.val = tmp.val;
 
-    hw->scl_low_period.scl_low_period = bus_cfg->scl_low - 1;
-    hw->scl_high_period.scl_high_period = bus_cfg->scl_high;
-    hw->scl_high_period.scl_wait_high_period = bus_cfg->scl_wait_high;
+    dev->scl_low_period.scl_low_period = half_cycle - 1;
+    dev->scl_high_period.scl_high_period = scl_high;
+    dev->scl_high_period.scl_wait_high_period = scl_wait_high;
+
     //sda sample
-    hw->sda_hold.sda_hold_time = bus_cfg->sda_hold - 1;
-    hw->sda_sample.sda_sample_time = bus_cfg->sda_sample - 1;
+    dev->sda_hold.sda_hold_time = sda_hold - 1;
+    dev->sda_sample.sda_sample_time = sda_sample - 1;
     //setup
-    hw->scl_rstart_setup.scl_rstart_setup_time = bus_cfg->setup - 1;
-    hw->scl_stop_setup.scl_stop_setup_time = bus_cfg->setup - 1;
+    dev->scl_rstart_setup.scl_rstart_setup_time = half_cycle - 1;
+    dev->scl_stop_setup.scl_stop_setup_time = half_cycle - 1;
     //hold
-    hw->scl_start_hold.scl_start_hold_time = bus_cfg->hold - 1;
-    hw->scl_stop_hold.scl_stop_hold_time = bus_cfg->hold - 1;
-    hw->to.time_out_value = bus_cfg->tout;
-    hw->to.time_out_en = 1;
+    dev->scl_start_hold.scl_start_hold_time = half_cycle - 1;
+    dev->scl_stop_hold.scl_stop_hold_time = half_cycle - 1;
+
+    //default we set the timeout value to about 10 bus cycles
+    // log(20*half_cycle)/log(2) = log(half_cycle)/log(2) +  log(20)/log(2)
+    uint32_t tout = (int)(sizeof(half_cycle) * 8 - __builtin_clz(5 * half_cycle)) + 2;
+
+    dev->to.time_out_value = tout;
+    dev->to.time_out_en = 1;
 }
 
 void init_i2c(const i2c_cfg_t * cfg)
@@ -110,7 +82,7 @@ void init_i2c(const i2c_cfg_t * cfg)
 
     pclk_ctrl(_cfg->i2c_pclk, 1);
 
-    calc_bus_clk();
+    calc_bus_clk(_cfg->dev, _cfg->freq);
 
     _cfg->dev->ctr.conf_upgate = 1;
 
