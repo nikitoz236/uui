1513d5021fce02037e15c7df507200aac55a9ec7
Nikita Kulikov <nikitoz236@gmail.com>
2026-01-05 12:30:33 +0300

    ignore debug print to usb cdc acm when port not open - detect via in token irq

--- lib/hw/esp32/periph/dbg_usb_cdc_acm.c
+++ lib/hw/esp32/periph/dbg_usb_cdc_acm.c
@@ -1,7 +1,19 @@
 #include "soc/usb_serial_jtag_struct.h"
 
+// volatile uint32_t usb_write_cnt = 0;
+
 void dbg_usb_cdc_acm_tx(const char * s, unsigned len)
 {
+    // проверка что кабель подключен и порт открыт
+    if (USB_SERIAL_JTAG.int_raw.in_token_rec_in_ep1_int_raw) {
+        USB_SERIAL_JTAG.int_clr.in_token_rec_in_ep1_int_clr = 1;
+    } else {
+        // usb_write_cnt = 0;
+        return;
+    }
+
+    // usb_write_cnt += len;
+
     while (len--) {
         while (USB_SERIAL_JTAG.ep1_conf.serial_in_ep_data_free == 0) {};
         USB_SERIAL_JTAG.ep1.rdwr_byte = *s++;
