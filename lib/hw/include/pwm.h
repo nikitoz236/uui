deee208cddd01b7a4e0accb214899a73fc83c1d7
Nikita Kulikov <nikitoz236@gmail.com>
2026-01-07 11:54:17 +0300

    pwm interface fix typo arg cgf -> cfg

--- lib/hw/include/pwm.h
+++ lib/hw/include/pwm.h
@@ -3,6 +3,6 @@
 struct pwm_cfg;
 typedef const struct pwm_cfg pwm_cfg_t;
 
-void pwm_set_freq(const pwm_cfg_t * cgf, unsigned f);
-void pwm_set_ccr(const pwm_cfg_t * cgf, unsigned val);
-void init_pwm(const pwm_cfg_t * cgf);
+void pwm_set_freq(const pwm_cfg_t * cfg, unsigned f);
+void pwm_set_ccr(const pwm_cfg_t * cfg, unsigned val);
+void init_pwm(const pwm_cfg_t * cfg);
