0699c0e7a2b868c016a62568803f4e31c53e7b58
Nikita Kulikov <nikitoz236@gmail.com>
2026-03-08 15:45:03 +0300

    draw frame + EDGE_COUNT

--- lib/graphic/forms/forms_edge.h
+++ lib/graphic/forms/forms_edge.h
@@ -5,5 +5,6 @@ typedef enum {
     EDGE_U,
     EDGE_D,
     EDGE_L = EDGE_U,
-    EDGE_R = EDGE_D
+    EDGE_R = EDGE_D,
+    EDGE_COUNT
 } form_edge_t;
--- lib/graphic/lcd_color/draw_color.h
+++ lib/graphic/lcd_color/draw_color.h
@@ -1,8 +1,21 @@
 #include "api_lcd_color.h"
 #include "forms.h"
 #include "color_type.h"
+#include "forms_split.h"
 
 static inline void draw_color_form(form_t * f, lcd_color_t color)
 {
     lcd_rect(f->p.x, f->p.y, f->s.w, f->s.h, color);
 };
+
+static inline void draw_frame(form_t * f, unsigned width, lcd_color_t color)
+{
+    form_t line;
+    for (dimension_t d = 0; d < DIMENSION_COUNT; d++) {
+        for (form_edge_t e = 0; e < EDGE_COUNT; e++) {
+            line = *f;
+            form_reduce(&line, d, e, width);
+            draw_color_form(&line, color);
+        }
+    }
+}
