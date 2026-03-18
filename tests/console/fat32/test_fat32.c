85aa22cf47114a8e67dc0245ecbb96f60d5bbbb0
Nikita Kulikov <nikitoz236@gmail.com>
2026-03-18 18:30:11 +0300

    test_fat32.c

---
+++ tests/console/fat32/test_fat32.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#include "dp.h"
+
+void sector_dump(uint8_t * ptr, uint32_t start_address, unsigned len)
+{
+    
+}
+
+int main()
+{
+    dpn("test fat32");
+
+    return 0;
+}
