1671df562b0385755772135cc0f9bb5f2b1cdf4e
Nikita Kulikov <nikitoz236@gmail.com>
2026-03-20 05:16:27 +0300

    utf tests - zero counted

--- lib/common/utf.c
+++ lib/common/utf.c
@@ -1,11 +1,14 @@
 #include "utf.h"
 
+#define DP_OFF
+#define DP_NAME "UTF"
+#include "dp.h"
+
 unsigned utf16_to_utf8(uint8_t * dst, unsigned dst_max, const uint16_t * src, unsigned src_len)
 {
     unsigned si = 0;
     unsigned di = 0;
     while (1) {
-        uint16_t ch;
         if (src_len != 0) {
             if (si >= src_len) {
                 break;
@@ -14,38 +17,40 @@ unsigned utf16_to_utf8(uint8_t * dst, unsigned dst_max, const uint16_t * src, un
         if (dst_max == 0) {
             break;
         }
-        ch = src[si++];
+        utf16_t c_u16 = src[si++];
 
-        if (ch == 0) {
-            break;
-        }
-
-        if (ch >= 0xD800 && ch <= 0xDFFF) {
+        if (c_u16 >= 0xD800 && c_u16 <= 0xDFFF) {
             if (di < dst_max) {
                 dst[di++] = '@';
             }
             /* Любой суррогат превращается в одну '@', пропускать только один 16-битный символ */
         } else {
-            if (ch <= 0x7F) {
-                dst[di++] = (uint8_t)ch;
-            } else if (ch <= 0x7FF) {
+            if (c_u16 <= 0x7F) {
+                dst[di++] = (uint8_t)c_u16;
+                dp("1 byte encode, char"); dpx(c_u16, 2); dp(" di "); dpd(di, 4); dp(" si "); dpd(si, 4); dn();
+            } else if (c_u16 <= 0x7FF) {
                 if (dst_max < 2) {
                     break;
                 }
-                dst[di++] = (uint8_t)(0xC0 | ((ch >> 6) & 0x1F));
-                dst[di++] = (uint8_t)(0x80 | (ch & 0x3F));
+                dst[di++] = (uint8_t)(0xC0 | ((c_u16 >> 6) & 0x1F));
+                dst[di++] = (uint8_t)(0x80 | (c_u16 & 0x3F));
                 dst_max--;
             } else {
                 if (dst_max < 3) {
                     break;
                 }
-                dst[di++] = (uint8_t)(0xE0 | ((ch >> 12) & 0x0F));
-                dst[di++] = (uint8_t)(0x80 | ((ch >> 6) & 0x3F));
-                dst[di++] = (uint8_t)(0x80 | (ch & 0x3F));
+                dst[di++] = (uint8_t)(0xE0 | ((c_u16 >> 12) & 0x0F));
+                dst[di++] = (uint8_t)(0x80 | ((c_u16 >> 6) & 0x3F));
+                dst[di++] = (uint8_t)(0x80 | (c_u16 & 0x3F));
                 dst_max -= 2;
             }
         }
         dst_max--;
+
+        if (c_u16 == 0) {
+            break;
+            dp("0 detected, di "); dpd(di, 4); dp(" si "); dpd(si, 4); dn();
+        }
     }
     return di;
 }
--- tests/console/utf8_test/test_utf8.c
+++ tests/console/utf8_test/test_utf8.c
@@ -16,17 +16,17 @@ struct {
     // 1
     { .s = (utf16_t []){ 'h', 'e', 'y' }, .sl = 2, .dl = 10, .ref = "he", .ref_len = 2 },
     // 2
-    { .s = (utf16_t []){ 'h', 'e', 'y', 0 }, .sl = 0, .dl = 10, .ref = "hey", .ref_len = 3 },
+    { .s = (utf16_t []){ 'h', 'e', 'y', 0 }, .sl = 0, .dl = 10, .ref = "hey", .ref_len = 4 },
     // 3
     { .s = (utf16_t []){ 'h', 'e', 'y', 0 }, .sl = 0, .dl = 2, .ref = "hey", .ref_len = 2 },
     // 4 - Русские буквы (кириллица) "Привет"
-    { .s = (utf16_t []){ 0x041F, 0x0440, 0x0438, 0x0432, 0x0435, 0x0442, 0 }, .sl = 0, .dl = 16, .ref = "\xD0\x9F\xD1\x80\xD0\xB8\xD0\xB2\xD0\xB5\xD1\x82", .ref_len = 12 },
+    { .s = (utf16_t []){ 0x041F, 0x0440, 0x0438, 0x0432, 0x0435, 0x0442, 0 }, .sl = 0, .dl = 16, .ref = "\xD0\x9F\xD1\x80\xD0\xB8\xD0\xB2\xD0\xB5\xD1\x82", .ref_len = 13 },
     // 5 - Русское "ёж" + latin "abc"
-    { .s = (utf16_t []){ 0x0451, 0x0436, 'a', 'b', 'c', 0 }, .sl = 0, .dl = 16, .ref = "\xD1\x91\xD0\xB6""abc", .ref_len = 7 },
+    { .s = (utf16_t []){ 0x0451, 0x0436, 'a', 'b', 'c', 0 }, .sl = 0, .dl = 16, .ref = "\xD1\x91\xD0\xB6""abc", .ref_len = 8 },
     // 6 - Суррогатная пара (high surrogate)
-    { .s = (utf16_t []){ 0xD834, 0xDD1E, 0 }, .sl = 0, .dl = 8, .ref = "@@", .ref_len = 2 },
+    { .s = (utf16_t []){ 0xD834, 0xDD1E, 0 }, .sl = 0, .dl = 8, .ref = "@@", .ref_len = 3 },
     // 7 - Одиночный суррогат (low surrogate)
-    { .s = (utf16_t []){ 0xDD1E, 0 }, .sl = 0, .dl = 8, .ref = "@", .ref_len = 1 },
+    { .s = (utf16_t []){ 0xDD1E, 0 }, .sl = 0, .dl = 8, .ref = "@", .ref_len = 2 },
 };
 
 unsigned do_test(utf16_t * s, unsigned sl, unsigned dl, utf8_t * ref, unsigned ref_len)
