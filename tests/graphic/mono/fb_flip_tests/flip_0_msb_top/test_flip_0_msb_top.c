12f9078f01460fcae89100ee5bc7212b9f5a30bb
Nikita Kulikov <nikitoz236@gmail.com>
2026-04-27 22:22:23 +0300

    fb flip tests

---
+++ tests/graphic/mono/fb_flip_tests/flip_0_msb_top/test_flip_0_msb_top.c
@@ -0,0 +1,31 @@
+#include "emu_graphic.h"
+#include "emu_fb_lcd.h"
+#include "lcd_fb.h"
+#include "dp.h"
+
+LCD_FB_CREATE(96, 68);
+
+int main()
+{
+    dpn("test fb mono - LCD_TOP_MSB, no LCD_FLIP_180");
+
+    emu_fb_lcd_cfg_t cfg = {
+        .scale = 6,
+        .px_gap = 1,
+        .border = 10,
+        .bg_color = 0x5f7537,
+        .on_color = 0,
+        .off_color = 0x526137,
+        .msb_top = 0,
+        .flip_180 = 0,
+    };
+    emu_lcd_mono_init(&cfg);
+
+    lcd_clear();
+    dpn(" rect horizontal. left top (5, 10) - size (40, 20)");
+    lcd_rect(5, 10, 40, 20, 1);
+    lcd_refresh();
+
+    emu_graphic_loop(0);
+    return 0;
+}
